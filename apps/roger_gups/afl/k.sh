729d8d52885a9e65773c7b0f86dbbc36ff4533bb
Roger Golliver, INTEL <ragolliv>
2012-06-11 16:53:52 +0000

    Added k.sh for AFL which does run and valgrind check. Roger
    git-svn-id: https://uhpc.nbgisp.com/svn/runnemede/trunk@2810 b8300b04-83cd-4eb7-bcdf-78d2b5523b00

---
+++ apps/roger_gups/afl/k.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+#next four lines just compile and link to make sure all of the define combinations work
+#make RUN_SUFFIX=DRAM-RMW RAG_CACHE=0 RAG_ATOMIC=0 CACHE_RUN=0 clean gups gups.s
+#make RUN_SUFFIX=DRAM-RMW RAG_CACHE=0 RAG_ATOMIC=1 CACHE_RUN=0 clean gups gups.s
+#make RUN_SUFFIX=DRAM-RMW RAG_CACHE=0 RAG_ATOMIC=0 CACHE_RUN=1 clean gups gups.s
+#make RUN_SUFFIX=DRAM-RMW RAG_CACHE=0 RAG_ATOMIC=1 CACHE_RUN=1 clean gups gups.s
+#exit
+#these lines run the four GUPS options
+make RUN_SUFFIX=DRAM--RMW RAG_CACHE=0 RAG_ATOMIC=0 CACHE_RUN=0 clean gups gups.s run check
+make RUN_SUFFIX=DRAM--AMO RAG_CACHE=0 RAG_ATOMIC=1 CACHE_RUN=0 clean gups gups.s run check
+make RUN_SUFFIX=Cache-RMW RAG_CACHE=0 RAG_ATOMIC=0 CACHE_RUN=1 clean gups gups.s run check
+make RUN_SUFFIX=Cache-AMO RAG_CACHE=0 RAG_ATOMIC=1 CACHE_RUN=1 clean gups gups.s run check
