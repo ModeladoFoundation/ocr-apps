20a1aecc59aeeaa65886491b834a0be17943456e
Roger A Golliver <roger.a.golliver@gmail.com>
2013-06-13 12:28:25 -0700

    Roger -- fixed scripts to account for multiple XE's outputing results.

--- apps/roger_lulesh/fsim/here.sh
+++ apps/roger_lulesh/fsim/here.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
-egrep -e '(time\ =|Final)' /tmp/roger-fsim.????/fsim-strip.logs.lulesh.log.brd00.chp00.unt00.blk00.CE.00 \
-| sed -e 's/CONSOLE: XE. >>> //' >/tmp/j
+egrep -e '(time\ =|Final)' logs/lulesh.log.brd00.chp00.unt00.blk00.CE.00 \
+| sed -e 's/^CONSOLE: XE. >>> //' >/tmp/j
 ~/xstack/apps/roger_lulesh/utils/hex2sci </tmp/j >/tmp/jj
 wc /tmp/j /tmp/jj
 diff ~roger/xstack/apps/roger_lulesh/utils/gold.5.hex /tmp/j  | head -5 
