b0432f56f85b4988226f284fad81cc14e56fca01
Roger Golliver <ragollix@odin-001.jf.intel.com>
2013-08-04 11:15:45 -0700

    Roger -- OCR another fix for exit status

--- apps/roger_lulesh/ocr-src/RAG.h
+++ apps/roger_lulesh/ocr-src/RAG.h
@@ -121,7 +121,7 @@ struct DomainObject_t {
 #define HC_UPC_CONST const
 #define HAB_CONST const
 #define SHARED
-#define EXIT(code) { /*printf("RAG: exit(%d)\n",code);*/ ocrShutdown(); exit(0); }
+#define EXIT(code) { /*printf("RAG: exit(%d)\n",code);*/ ocrShutdown(); exit(code); }
 
 #include "ocr.h"
 
