8be8ad36f88c549f19b597c36cc803f5cce0e76a
Bala S <bala.seshasayee@intel.com>
2014-12-04 16:26:32 -0800

    Fix sqrt() for TG
    Get rid of temporary algorithm and simply use
    the instruction, really [closes bug #140]
    
    Change-Id: I49163e6e87c9ada876a5c4f0bad95538459b792c

--- apps/libs/src/libc/tg-includes/math.h
+++ apps/libs/src/libc/tg-includes/math.h
@@ -2,7 +2,6 @@
 #define _MATH_H
 
 #define M_PI           3.14159265358979323846
-#define PRECISION      1e-10
 
 inline void  __attribute__((always_inline)) sincosf(float x, float* sinx, float* cosx) {
     __asm__ (
@@ -37,16 +36,15 @@ inline double __attribute__((always_inline)) fabs(double x) {
     return x;
 }
 
-/* Below is a temporary implementation, needs to be refined (bug #140) */
-
 inline double __attribute__((always_inline)) sqrt(double x) {
-    double ans = 1, old = 0;
-    while(fabs(old-ans) >= PRECISION)
-    {
-        old = ans;
-        ans = ((x/ans) + ans) / 2;
-    }
-    return ans;
+    double result;
+    __asm__ (
+        "rcpsqrtF64 %0, %1\n\t"
+        : "=r" (result)
+        : "r" (x)
+        :
+    );
+    return 1/result;
 }
 
 #endif //_MATH_H
