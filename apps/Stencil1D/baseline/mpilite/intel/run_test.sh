067e1b9ae74fb890f4fbaea8091fd8b6cd7e9fb6
mwdavis <mark.davis@intel.com>
2015-03-27 12:34:44 -0700

    MPILite version of stencil
    In Stencil1D/baseline/mpilite/intel/
    run_test.sh does the version with MPI_Isend
    run_send_test.sh uses MPI_Send
    Make sure you have OCR_INSTALL defined

---
+++ apps/Stencil1D/baseline/mpilite/intel/run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+[ $# -ne 2 ] && { echo "Usage: $0 NX NITER "; exit 1; }
+make PROG=stencil clean
+make PROG=stencil compile
+echo "Testing NX = $1, NITER = $2"
+make PROG=stencil ARGS="$1 $2" run 2>1 |  tee log.$1
+#./stencil_send.exe -r 4 $1 $2  2>&1 | tee log_send.$1
+ 
