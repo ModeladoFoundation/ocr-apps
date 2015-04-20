bfc6064ae953a4aa83bea6f811dde5f9d29df698
mwdavis <mark.davis@intel.com>
2015-04-20 09:54:10 -0700

    Rename mpi->mpilite in 2 places in CoMD
    refactored/mpilite; mpilite/build_and_run/mpilite
    changed examples/mpi-runapp-SS.sh to use 'mpilite" name in paths.
    
    Change-Id: I7286330b45f2afc2bfa0ae91d7c970eb847bf053

--- apps/CoMD/refactored/mpilite/examples/mpi-runapp-SS.sh
+++ apps/CoMD/refactored/mpilite/examples/mpi-runapp-SS.sh
@@ -2,10 +2,10 @@
 
 
 HERE=$PWD
-cd ../build_and_run/mpi
+cd ../build_and_run/mpilite
 make OCR_TYPE=x86-pthread-x86 CoMD
 
 cd $HERE
 
-CoMD=../build_and_run/mpi/CoMD
+CoMD=../build_and_run/mpilite/CoMD
 source mpi-strongScaling.inc
