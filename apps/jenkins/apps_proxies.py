3c9344019b039ae96006da0d17791b27ec064856
Gabriele Jost <gabriele.jost@intel.com>
2015-05-05 10:32:59 -0700

    add tempest to gerrit and include in jenkins
    Change-Id: I8a42e64cc03e5ba96d1934fd49d1dfd922b8aea0

--- apps/jenkins/apps_proxies.py
+++ apps/jenkins/apps_proxies.py
@@ -81,3 +81,28 @@ job_ocr_run_kernel_comdsdsc_x86_remote_regression = {
                   'WORKLOAD_ARGS': '-x 5 -y 5 -z 5 -d ${JJOB_SHARED_HOME}/xstack/apps/CoMD/datasets/pots',
                   'WORKLOAD_INSTALL_ROOT': '${JJOB_SHARED_HOME}/xstack/apps/CoMD/refactored/ocr/sdsc/install'}
 }
+job_ocr_build_kernel_comdllnl_x86_regression = {
+    'name': 'ocr-build-kernel-tempestsw2lite-x86-regression',
+    'depends': ('ocr-build-x86-pthread-x86',),
+    'jobtype': 'ocr-build-kernel-regression',
+    'run-args': 'tempestsw2lite x86-pthread-x86',
+    'sandbox': ('shared','inherit0'),
+    'env-vars': { 'APPS_ROOT': '${JJOB_SHARED_HOME}/xstack/apps',
+                  'APPS_LIBS_ROOT': '${JJOB_SHARED_HOME}/xstack/apps/libs/x86',
+                  'WORKLOAD_SRC': '${JJOB_SHARED_HOME}/xstack/apps/tempest/refactored/mpilite/intel/test/shallowwater_sphere',
+                  'WORKLOAD_BUILD_ROOT': '${JJOB_PRIVATE_HOME}/xstack/apps/CoMD/refactored/ocr/intel/test/shallowwater_sphere/build',
+                  'WORKLOAD_INSTALL_ROOT': '${JJOB_SHARED_HOME}/xstack/apps/CoMD/refactored/ocr/intel/test/shallowwater_sphere/install'}
+}
+
+job_ocr_run_kernel_tempestsw2lite = {
+    'name': 'ocr-run-kernel-tempestsw2lite-x86-remote-regression',
+    'depends': ('ocr-build-kernel-tempestite-x86-regression',),
+    'jobtype': 'ocr-run-kernel-remote-regression',
+    'run-args': 'SWTest2 x86-pthread-x86 ocr-run-kernel-comdllnl-x86-remote-regression 10',
+    'sandbox': ('shared','inherit0'),
+    'env-vars': { 'APPS_ROOT': '${JJOB_SHARED_HOME}/xstack/apps',
+                  'APPS_LIBS_ROOT': '${JJOB_SHARED_HOME}/xstack/apps/libs/x86',
+                  'WORKLOAD_SRC': '${JJOB_SHARED_HOME}/xstack/apps/CoMD/refactored/mpilite/test/shallowwater_sphere',
+                  'WORKLOAD_ARGS': '-r 4 -t 1600',
+                  'WORKLOAD_INSTALL_ROOT': '${JJOB_SHARED_HOME}/xstack/apps/CoMD/mpilite/test/shallowwater_sphere/install'}
+}
