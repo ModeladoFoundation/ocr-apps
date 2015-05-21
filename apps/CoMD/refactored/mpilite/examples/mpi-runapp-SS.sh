894a6d4dac495108d7569ba073fa4a081c4d7be5
mwdavis <mark.davis@intel.com>
2015-05-21 12:35:55 -0700

    comd: remove CoMD_info.h from src
    Now that makefile is creating it correctly, don't need one in src-mpi.
    in the scripts in examples, define OCR_INSTALL if it's not defined, and
    use it in LD_LIB_PATH.
    
    Change-Id: I2bebc54b42aa3eb860d857027b63b4dafedd2b62

--- apps/CoMD/refactored/mpilite/examples/mpi-runapp-SS.sh
+++ apps/CoMD/refactored/mpilite/examples/mpi-runapp-SS.sh
@@ -8,4 +8,9 @@ make OCR_TYPE=x86-pthread-x86 CC=icc install
 cd $HERE
 
 CoMD=../build_and_run/mpilite/build/x86-pthread-x86/comd-mpi
+
+if [[ "$OCR_INSTALL"1 == 1 ]]; then
+    OCR_INSTALL=$APPS_ROOT/../ocr/install/x86-pthread-x86
+fi
+
 source mpi-strongScaling.inc
