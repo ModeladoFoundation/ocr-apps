69d5ee297af1f4efca1e9254ec990a402ab3964e
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2015-08-10 02:07:45 -0700

    Priority-based scheduler works with the printf example
    Change-Id: Iea8d54d4fc3e85716f2249437f4328462449e558

--- apps/examples/printf/printf.c
+++ apps/examples/printf/printf.c
@@ -1,7 +1,47 @@
 #include "ocr.h"
+#include "utils/bin-heap.h"
+
+ocrGuid_t f( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
+    const u64 n = paramv[0];
+    PRINTF("Hello from %lu\n", n);
+    if (n == 0) ocrShutdown();
+    return NULL_GUID;
+}
 
 ocrGuid_t mainEdt ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
+    //////////////////////////////////////
+    ocrPolicyDomain_t *pd;
+    getCurrentEnv(&pd, NULL, NULL, NULL);
+    binHeap_t *heap = newBinHeap(pd, LOCKED_BIN_HEAP);
+    u8 *p = (u8*)1;
+    int i;
+    for (i=0; i<30; i+=3) {
+        const int n = i%10;
+        heap->push(heap, p+n, (u32)(u64)p+n, 0);
+        PRINTF("%p\n", p+n);
+    }
+    PRINTF("==\n", p+i);
+    for (i=0; i<10; i++) {
+        u8 *q = heap->pop(heap, 0);
+        PRINTF("%p\n", q);
+    }
     PRINTF("Hello from mainEdt()\n");
-    ocrShutdown();
-    return 0;
+    /////////////////////////////////////
+    ocrGuid_t template, edt, event;
+    ocrEventCreate(&event, OCR_EVENT_ONCE_T, EVT_PROP_NONE);
+    ocrEdtTemplateCreate(&template, f, 1, 1);
+    for (i=0; i<30; i+=3) {
+        u64 n = i%10;
+        ocrEdtCreate(&edt, template, 1, &n, 1, &event, EDT_PROP_NONE, NULL_GUID, NULL);
+        { // OCR hints
+            ocrHint_t _stepHints;
+            ocrHintInit(&_stepHints, OCR_HINT_EDT_T);
+            u64 _hintVal = n;
+            ocrSetHintValue(&_stepHints, OCR_HINT_EDT_PRIORITY, _hintVal);
+            ocrSetHint(edt, &_stepHints);
+        }
+        PRINTF("CREATE %lu\n", n);
+    }
+    ocrEventSatisfy(event, NULL_GUID);
+    return NULL_GUID;
 }
