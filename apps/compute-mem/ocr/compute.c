e25a017583663b3c30135bfe6d33341cd6967f22
Kavitha Chandrasekar <kchandrasekar@gatech.edu>
2015-08-18 08:12:26 -0700

    Adding HT heuristic for compute and memory intensive tasks

--- apps/compute-mem/ocr/compute.c
+++ apps/compute-mem/ocr/compute.c
@@ -12,17 +12,19 @@
 
 #define CONSTANT_LARGE_NUMBER 1000
 
-#define THRESHOLD 100//180
+#define THRESHOLD_COMP 30//0//180
 #include "common.h"
 
 ocrGuid_t templateComputeSpawner;
-ocrGuid_t event_guid[THRESHOLD];
+ocrGuid_t event_guid[THRESHOLD_COMP];
 
 
 int iteration;
 
+ocrHint_t compute_app_type_hint;
+
 ocrGuid_t wrap_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
-  PRINTF("\nFinishing compute tasks");
+  PRINTF("\nFinishing compute tasks"); fflush(stdout);
     if(instances==0)
       instances++;
     else
@@ -32,7 +34,7 @@ ocrGuid_t wrap_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
 ocrGuid_t spawn_and_compute_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
     
     u64 i, j, k;
-
+    PRINTF("Compute task\n");fflush(stdout);
 
 //Spawn more tasks before starting computation to generate enough parallelism
  
@@ -51,8 +53,17 @@ ocrGuid_t spawn_and_compute_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_
 
     u64 tag = 1;
 
-    if(index!=THRESHOLD-1)
-        ocrEdtCreate/*Tag*/(&computeEdtGuid, templateComputeSpawner, 1, func_args_new, 0, NULL, PROPERTIES, affinity, NULL);//, tag);
+    if(index!=THRESHOLD_COMP-1){
+        ocrEdtCreate/*Tag*/(&computeEdtGuid, templateComputeSpawner, 1, func_args_new, 1, NULL, PROPERTIES, affinity, NULL);//, tag);
+        ocrSetHint(computeEdtGuid, &compute_app_type_hint);
+
+        ocrGuid_t depv_guid;
+        void* db;
+        ocrDbCreate(&depv_guid, &db,
+           sizeof(double), FLAGS, NULL_GUID, NO_ALLOC);
+        ocrAddDependence(depv_guid, computeEdtGuid, 0, DB_DEFAULT_MODE);
+
+    }
 
 
 
@@ -90,21 +101,35 @@ ocrGuid_t mainEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[]) {
 
     ocrGuid_t affinity = NULL_GUID;
     ocrGuid_t templateWrap;
-    ocrEdtTemplateCreate(&templateWrap, wrap_task, 0, THRESHOLD);
+    ocrEdtTemplateCreate(&templateWrap, wrap_task, 0, THRESHOLD_COMP);
 
+    u64 app_tag = 0;
+
+    ocrHintInit(&compute_app_type_hint, OCR_HINT_EDT_T);
+    ocrSetHintValue(&compute_app_type_hint, OCR_HINT_EDT_TASK_TYPE, app_tag);
+ 
     ocrGuid_t wrapEdtGuid;
     u64 tag = 1;
-    ocrEdtCreate/*Tag*/(&wrapEdtGuid, templateWrap, 0, NULL, THRESHOLD, NULL, PROPERTIES, affinity, NULL);//, tag);
+    ocrEdtCreate/*Tag*/(&wrapEdtGuid, templateWrap, 0, NULL, THRESHOLD_COMP, NULL, PROPERTIES, affinity, NULL);//, tag);
     
     u64 i;
-    for(i=0;i<THRESHOLD;i++){
+    for(i=0;i<THRESHOLD_COMP;i++){
         ocrEventCreate(&(event_guid[i]), OCR_EVENT_STICKY_T, TRUE);
         ocrAddDependence(event_guid[i], wrapEdtGuid, i, DB_MODE_ITW);
     }
 
-    ocrEdtTemplateCreate(&templateComputeSpawner, spawn_and_compute_task, 1, 0);
+    ocrEdtTemplateCreate(&templateComputeSpawner, spawn_and_compute_task, 1, 1);
    
     ocrGuid_t computeEdtGuid; 
-    ocrEdtCreate/*Tag*/(&computeEdtGuid, templateComputeSpawner, 1, func_args, 0, NULL, PROPERTIES, affinity, NULL);//, tag);
+    ocrEdtCreate/*Tag*/(&computeEdtGuid, templateComputeSpawner, 1, func_args, 1, NULL, PROPERTIES, affinity, NULL);//, tag);
+
+    ocrSetHint(computeEdtGuid, &compute_app_type_hint);
+
+    ocrGuid_t depv_guid;
+    void* db;
+    ocrDbCreate(&depv_guid, &db,
+           sizeof(double), FLAGS, NULL_GUID, NO_ALLOC);
+    ocrAddDependence(depv_guid, computeEdtGuid, 0, DB_DEFAULT_MODE);
+
     return NULL_GUID;
 }
--- apps/compute-mem/ocr/memory.c
+++ apps/compute-mem/ocr/memory.c
@@ -15,14 +15,16 @@
 
 #define CONSTANT_LARGE_NUMBER 1200000
 
-#define THRESHOLD 1500
+#define THRESHOLD 15//00
 
 ocrGuid_t templateMemSpawner;
 ocrGuid_t event_mem_guid[THRESHOLD];
-
+ocrHint_t memory_app_type_hint;
 int iteration;
+
+
 ocrGuid_t wrap_mem_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
-  PRINTF("\nFinishing memory app");
+  PRINTF("\nFinishing memory app (instances=%llu)", instances);
     if(instances==0)
       instances++;
     else
@@ -31,7 +33,7 @@ ocrGuid_t wrap_mem_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[])
 
 ocrGuid_t spawn_and_mem_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
 
-//    PRINTF("\nMemory spawned EDT");  
+    PRINTF("\nMemory spawned EDT");  fflush(stdout);
     u64 j, k;
 
     long i;
@@ -48,11 +50,17 @@ ocrGuid_t spawn_and_mem_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t de
 
     ocrGuid_t memoryEdtGuid;
       
-    u64 tag = 2;
-    tag = 1;
-    if(index!=THRESHOLD-1)
-        ocrEdtCreate/*Tag*/(&memoryEdtGuid, templateMemSpawner, 1, func_args_new, 0, NULL, PROPERTIES, affinity, NULL);//, tag);
-
+    if(index!=THRESHOLD-1){
+        ocrEdtCreate(&memoryEdtGuid, templateMemSpawner, 1, func_args_new, 1, NULL, PROPERTIES, affinity, NULL);//, tag);
+     
+        ocrSetHint(memoryEdtGuid, &memory_app_type_hint);
+        
+        ocrGuid_t depv_guid;
+        void* db;
+        ocrDbCreate(&depv_guid, &db,
+            sizeof(double), FLAGS, NULL_GUID, NO_ALLOC);
+        ocrAddDependence(depv_guid, memoryEdtGuid, 0, DB_DEFAULT_MODE);
+    }
 
     double sum[1];
     double* a = (double*)malloc(sizeof(double)*CONSTANT_LARGE_NUMBER);
@@ -85,7 +93,13 @@ ocrGuid_t spawn_and_mem_task ( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t de
 
 ocrGuid_t main2Edt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[]) {
 
-    PRINTF("\nMemory main EDT");
+    PRINTF("\nMemory main EDT");fflush(stdout);
+
+    u64 app_tag = 1;
+
+    ocrHintInit(&memory_app_type_hint, OCR_HINT_EDT_T);
+    ocrSetHintValue(&memory_app_type_hint, OCR_HINT_EDT_TASK_TYPE, app_tag);
+
 
     u64 func_args[1];
     func_args[0] = 0;
@@ -105,8 +119,18 @@ ocrGuid_t main2Edt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[]) {
         ocrAddDependence(event_mem_guid[i], wrapEdtGuid, i, DB_MODE_ITW);
     }
 
-    ocrEdtTemplateCreate(&templateMemSpawner, spawn_and_mem_task, 1, 0);
+    ocrSetHint(wrapEdtGuid, &memory_app_type_hint);
+
+    ocrEdtTemplateCreate(&templateMemSpawner, spawn_and_mem_task, 1, 1);
    
     ocrGuid_t memoryEdtGuid; 
-    ocrEdtCreate/*Tag*/(&memoryEdtGuid, templateMemSpawner, 1, func_args, 0, NULL, PROPERTIES, affinity, NULL);//, tag);
+    ocrEdtCreate/*Tag*/(&memoryEdtGuid, templateMemSpawner, 1, func_args, 1, NULL, PROPERTIES, affinity, NULL);//, tag);
+
+    ocrSetHint(memoryEdtGuid, &memory_app_type_hint);
+
+    ocrGuid_t depv_guid;
+    void* db;
+    ocrDbCreate(&depv_guid, &db,
+       sizeof(double), FLAGS, NULL_GUID, NO_ALLOC);
+    ocrAddDependence(depv_guid, memoryEdtGuid, 0, DB_DEFAULT_MODE);
 }
