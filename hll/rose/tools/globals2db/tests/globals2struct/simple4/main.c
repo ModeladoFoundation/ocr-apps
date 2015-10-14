c360f6009735d546ad3aaa35c759480bafd47efa
Gail Lyons <gail.lyons@intel.com>
2015-10-14 11:12:51 -0700

    Implemented code improvements recommended by Pei-Hung Lin @ LLNL.
    Many warnings were emitted by globals2db.  During the Applications
    Workshop, I met with Pei-Hung from LLNL.  Pei-Hung works on ROSE.
    He recommended reordering some of the API calls.  Some warnings
    are still emitted, but this is a step in the right direction.
    
    Also added a test which I sent to Pei-Hung.
    
    Change-Id: Ie16a84c1b2823d7566de62715bfd938f7f6872af

---
+++ hll/rose/tools/globals2db/tests/globals2struct/simple4/main.c
@@ -0,0 +1,18 @@
+// This file is subject to the license agreement located in the file LICENSE
+// and cannot be distributed without it. This notice cannot be
+// removed or modified.
+
+// global
+float val[64];
+
+int main (int argc, char **argv) {
+    int i;
+
+    // initialization
+    for (i = 0; i <= 5 ; i++) {
+        val [i] = 0.;
+    }
+    return (0);
+}
+
+
