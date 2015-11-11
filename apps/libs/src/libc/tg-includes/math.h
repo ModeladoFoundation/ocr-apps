426903d7d233d64d23fcba39b5ae320a6f62797d
Bala S <bala.seshasayee@intel.com>
2015-11-11 14:48:33 -0800

    Update TG libs for 4.1.0
    Few fixes to TG apps libs for 4.1.0,
    both in assembly and memory for file I/O staging
    
    Change-Id: I890c6ebdd5be3b941b979fda5b0bb6483ab2ea42

--- apps/libs/src/libc/tg-includes/math.h
+++ apps/libs/src/libc/tg-includes/math.h
@@ -38,9 +38,7 @@ inline int  __attribute__((always_inline)) isInf(double x) {
 
 inline void  __attribute__((always_inline)) sincosf(float x, float* sinx, float* cosx) {
     __asm__ (
-        "sincosF32 %0, %2\n\t"
-        "or32 %1, %0, 0x0\n\t" /* cosine takes the lower 32 bits */
-        "slr64  %0, %0, 32\n\t"
+        "sincosF %0, %1, %2, 32\n\t"
         : "=r" (*sinx), "=r" (*cosx)
         : "r" (x)
         :
@@ -130,7 +128,7 @@ inline double __attribute__((always_inline)) log(double x) {
 }
 
 inline double __attribute__((always_inline)) sqrt(double x) {
-#if 1
+#if 0
     __asm__ (
         "rcpsqrtF64 %0, %1\n\t"
         "rcpF64 %0, %0\n\t"
@@ -138,10 +136,10 @@ inline double __attribute__((always_inline)) sqrt(double x) {
         : "r" (x)
         );
     return x;
-#elif 0
+#elif 1
     //TODO when 4.1.0 is available
     __asm__ (
-        "sqrtF %0, %0\n\t"
+        "sqrtF %0, %0, 64\n\t"
         : "=r" (x)
         : "r" (x)
         );
