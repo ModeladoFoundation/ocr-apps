831aa08f4bcd27801c1c971dfb4153d96734b390
Romain <romain.e.cledat@intel.com>
2016-01-04 04:14:08 -0800

    Repo split: Import changes from old runtime/master to apps
    These are the changes that were committed to runtime/master
    but not merged with apps/master but affect apps/ and the old
    ocr/examples
    
    Change-Id: I36faaab5ae7f410f8848d4e408a22ff31f89d27f

--- apps/jenkins/scripts/plotGraph.py
+++ apps/jenkins/scripts/plotGraph.py
@@ -19,15 +19,16 @@ import subprocess
 import sys
 import os
 import random
+
 #-------------------------------------------------------------------------------
 # Plots curve
 #-------------------------------------------------------------------------------
-def plotCurve(testname,stat,xrange, baseColor, ymax):
+def plotCurve(testname,stat,xrange, yMin, yMax, baseColor):
     plt.plot(xrange , stat,color = baseColor ,label=testname)
     plt.axhline(y=1,linestyle=':')
     plt.axvline(x=0)
     x1,x2,y1,y2 = plt.axis()
-    plt.axis((x1,x2,0,ymax))
+    plt.axis((x1,x2,float(yMin),float(yMax)))
 
 #-------------------------------------------------------------------------------
 # Plots markers
@@ -61,7 +62,7 @@ def plotLegends():
 #-------------------------------------------------------------------------------
 # Generates an image file containing plots of the data from stat file
 #-------------------------------------------------------------------------------
-def plotgraph(testname,buildList , exectimeList, ymax, baseColor = 'b'):
+def plotgraph(testname,buildList , exectimeList, yMin, yMax, baseColor = 'b'):
 
     stat          = exectimeList
     buildNo       = buildList
@@ -75,19 +76,20 @@ def plotgraph(testname,buildList , exectimeList, ymax, baseColor = 'b'):
     xlabel.xaxis.grid(True)
     plt.setp(xlabel.get_xticklabels(), rotation='vertical', fontsize=8)
     # Plot Curve
-    plotCurve(testname,stat,xaxisCount , baseColor , ymax)
+    plotCurve(testname,stat,xaxisCount,yMin,yMax,baseColor)
 
 def main():
-    if len(sys.argv) !=7 :
-        print("ERROR ! Correct Usage :python plotGraph.py inputStatfile plotName xlabel ylabel outputFileName yMaxValue\n")
+    if len(sys.argv) !=8 :
+        print("ERROR ! Correct Usage :python plotGraph.py inputStatfile plotName xlabel ylabel outputFileName\n")
         sys.exit(1)
 
     iFilePath    = sys.argv[1]
     plotname     = sys.argv[2]
     xlabel       = sys.argv[3]
     ylabel       = sys.argv[4]
-    oFilePath    = sys.argv[5]
-    ymax         = int(sys.argv[6])
+    yMin         = sys.argv[5]
+    yMax         = sys.argv[6]
+    oFilePath    = sys.argv[7]
 
     fd = open(iFilePath, 'r')
     plotFileList = []
@@ -129,7 +131,7 @@ def main():
         buildStat = testStat[test]
         for build in buildList:
             modExectimeList.append(float(buildStat[build]) if (build in buildStat) else 0.0)
-        plotgraph(test,buildList,modExectimeList, ymax)
+        plotgraph(test,buildList,modExectimeList,yMin,yMax)
 
     # Plot legend
     plotLegends()
