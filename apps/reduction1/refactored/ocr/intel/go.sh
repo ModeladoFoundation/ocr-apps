d15841cd77e50ded9762e64416ad271a6f152f06
dsscott <david.s.scott@intel.com>
2016-01-20 15:37:54 -0800

    slight clean up of go.sh
    Change-Id: I9fee9af2d1ef97c6f66d009905a67dc669af0b9d

--- apps/reduction1/refactored/ocr/intel/go.sh
+++ apps/reduction1/refactored/ocr/intel/go.sh
@@ -1,3 +1,3 @@
 #export CONFIG_NUM_THREADS=1
 OCR_TYPE=x86  make clean uninstall run
-#OCR_TYPE=x86-mpi OCR_CONFIG=/home/dsscott/aug28/xstack/apps/reduction/refactored/ocr/intel/mpigenerated.cfg make clean run
+OCR_TYPE=x86-mpi make clean run
