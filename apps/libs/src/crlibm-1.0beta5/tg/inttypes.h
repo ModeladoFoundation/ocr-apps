3e0dc944dbb488cb4ac35f0408adac37b96df696
ragollix <roger.a.golliver@gmail.com>
2016-01-29 14:04:24 -0800

    Check-in of crlibm-1.0beta5 using latest OCR/TG versions
    Change-Id: I3a895a781200d7c56b47c6b10b8da2341a24e170

---
+++ apps/libs/src/crlibm-1.0beta5/tg/inttypes.h
@@ -0,0 +1,15 @@
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+/* degenerate inttypes.h */
+
+#ifdef TG_ARCH
+
+typedef int                int32_t;
+typedef unsigned int      uint32_t;
+typedef long int           int64_t;
+typedef unsigned long int uint64_t;
+
+#endif
+
+#endif
