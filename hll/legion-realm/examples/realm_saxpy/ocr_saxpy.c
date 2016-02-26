06292b0fd3ab89edd4463112c61fe175dae6aec1
Paul <sriraj@rice.edu>
2016-02-25 18:08:46 -0600

    adding an OCR equivalent implementation of realm_saxpy
    Reimplemting saxpy using OCR

---
+++ hll/legion-realm/examples/realm_saxpy/ocr_saxpy.c
@@ -0,0 +1,147 @@
+/* Copyright 2016 Rice University, Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//reimplementing the realm_saxpy_cpu using OCR
+//gcc -L${OCR_INSTALL}/lib -I${OCR_INSTALL}/include -locr_${OCR_TYPE} ocr_saxpy.c
+
+#include<assert.h>
+#include<stdlib.h>
+
+#include"ocr.h"
+
+struct param_type_t
+{
+	u64 count;
+	double val;
+};
+
+typedef struct param_type_t param_type;
+
+ocrGuid_t shutdown_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+	assert(argc == 0 && depc == 1);
+	ocrShutdown();
+        return NULL_GUID;
+}
+
+ocrGuid_t check_result_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+	assert(argc == 2 && depc == 3);
+	float *x_ptr = (float*)depv[0].ptr, *y_ptr = (float*)depv[1].ptr, *z_ptr = (float*)depv[2].ptr;
+	param_type params =*(param_type *)argv;
+	int count = params.count, success = 1, i;
+	float alpha = params.val;
+
+	for(i=0; i<count; i++){
+		float expected = alpha * x_ptr[i] + y_ptr[i];
+ 		float actual = z_ptr[i];
+ 		// FMAs are too acurate
+ 		float diff = (actual >= expected) ? actual - expected : expected - actual;
+ 		float relative = diff / expected;
+ 		if (relative > 1e-6)
+ 		{
+ 		  PRINTF("Expected: %.8g Actual: %.8g\n", expected, actual);
+ 		  success = 0;
+ 		  break;
+ 		}
+	}
+	if (success)
+	  PRINTF("SUCCESS!\n\n");
+	else
+	  PRINTF("FAILURE!\n\n");
+
+	return NULL_GUID;
+}
+
+ocrGuid_t cpu_saxpy_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+	assert(argc == 2 && depc == 2);
+	float *inst_z, *x_ptr = (float*)depv[0].ptr, *y_ptr = (float*)depv[1].ptr;
+	param_type params =*(param_type *)argv;
+	int count = params.count, i;
+	float alpha = params.val;
+	ocrGuid_t z_db_guid;
+	ocrDbCreate(&z_db_guid, (void **)(&inst_z), count*sizeof(float), DB_PROP_NONE, NULL_GUID, NO_ALLOC);
+
+	for(i=0; i<count; i++)
+		inst_z[i] = alpha * x_ptr[i] + y_ptr[i];
+	return z_db_guid;
+}
+
+ocrGuid_t fill_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+        assert(argc == 2 && depc ==1);
+	param_type params =*(param_type *)argv;
+	int count = params.count, i;
+	float init_val = params.val;
+	float *arr = (float*)(depv[0].ptr);
+	for(i=0; i<count; i++)
+		arr[i] = init_val;
+	return depv[0].guid;
+}
+
+ocrGuid_t fill(ocrGuid_t fill_edt_t, ocrGuid_t db_guid, int count, float init_value)
+{
+	param_type params = {count, init_value};
+	ocrGuid_t fill_edt, out_evt;
+	ocrEdtCreate(&fill_edt, fill_edt_t, EDT_PARAM_DEF, (u64*)&params, EDT_PARAM_DEF, &db_guid, EDT_PROP_NONE, NULL_GUID, &out_evt);
+	return out_evt;
+}
+
+ocrGuid_t top_level_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+	float *inst_x, *inst_y;
+        ocrGuid_t x_db_guid, y_db_guid;
+	int count = 16383;
+        ocrDbCreate(&x_db_guid, (void **)(&inst_x), count*sizeof(float), DB_PROP_NO_ACQUIRE, NULL_GUID, NO_ALLOC);
+        ocrDbCreate(&y_db_guid, (void **)(&inst_y), count*sizeof(float), DB_PROP_NO_ACQUIRE, NULL_GUID, NO_ALLOC);
+	PRINTF("Created data blocks - X:0x%lx Y:0x%lx\n", x_db_guid, y_db_guid);
+
+        float init_x_value = drand48();
+        float init_y_value = drand48();
+
+	ocrGuid_t fill_edt_t, out_evt[3];
+	ocrEdtTemplateCreate(&fill_edt_t, fill_func, 2, 1);
+	out_evt[0] = fill(fill_edt_t, x_db_guid, count, init_x_value);
+	out_evt[1] = fill(fill_edt_t, y_db_guid, count, init_y_value);
+	out_evt[2] = UNINITIALIZED_GUID;
+
+	param_type params = {count, drand48()};
+	ocrGuid_t cpu_edt_t, cpu_edt, cpu_out_evt;
+	ocrEdtTemplateCreate(&cpu_edt_t, cpu_saxpy_func, 2, 2);
+	ocrEdtCreate(&cpu_edt, cpu_edt_t, EDT_PARAM_DEF, (u64*)&params, EDT_PARAM_DEF, out_evt, EDT_PROP_NONE, NULL_GUID, &cpu_out_evt);
+
+	ocrGuid_t check_edt_t, check_edt, check_out_edt;
+	ocrEdtTemplateCreate(&check_edt_t, check_result_func, 2, 3);
+	ocrEdtCreate(&check_edt, check_edt_t, EDT_PARAM_DEF, (u64*)&params, EDT_PARAM_DEF, out_evt, EDT_PROP_NONE, NULL_GUID, &check_out_edt);
+	ocrAddDependence(cpu_out_evt, check_edt, 2, DB_MODE_RO);
+
+        return NULL_GUID;
+}
+
+ocrGuid_t mainEdt(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
+{
+	PRINTF("starting mainEdt\n");
+
+	ocrGuid_t top_edt_t, top_edt, top_out_evt;
+	ocrEdtTemplateCreate(&top_edt_t, top_level_func, 0, 0);
+	ocrEdtCreate(&top_edt, top_edt_t, EDT_PARAM_DEF, NULL, EDT_PARAM_DEF, NULL, EDT_PROP_FINISH, NULL_GUID, &top_out_evt);
+
+	ocrGuid_t sd_edt_t, sd_edt;
+	ocrEdtTemplateCreate(&sd_edt_t, shutdown_func, 0, 1);
+	ocrEdtCreate(&sd_edt, sd_edt_t, EDT_PARAM_DEF, NULL, EDT_PARAM_DEF, &top_out_evt, EDT_PROP_NONE, NULL_GUID, NULL);
+
+	return NULL_GUID;
+}
