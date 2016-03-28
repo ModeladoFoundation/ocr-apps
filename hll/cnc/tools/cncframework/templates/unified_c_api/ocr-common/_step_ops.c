d359a0fa058fe9a5c8140da1af414f63af784392
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-03-28 14:49:36 -0700

    CnC-OCR: Update to OCR 1.1.0 API
    Corresponds to b01d7ef in the cnc-framework repo on GitHub.
    
    Change-Id: I3c838be0c6ef4b9f3329459e6b42dbe7fe73caaa

--- hll/cnc/tools/cncframework/templates/unified_c_api/ocr-common/_step_ops.c
+++ hll/cnc/tools/cncframework/templates/unified_c_api/ocr-common/_step_ops.c
@@ -83,6 +83,17 @@ static void cncPrescribeInternal_{{stepfun.collName}}({{
           letting the compiler optimize it away to a constant in the trivial
           (rectangular) case might be possible. */ -#}
     u64 _depc = {{stepfun.inputCountExpr}} + {{ 1 if paramTag else 2 }};
+    ocrHint_t _hint;
+    {% if g.hasTuning('priority') -%}
+    ocrHint_t *const _hintPtr = &_hint;
+    if (!_cncCurrentEdtAffinityHint(_hintPtr)) {
+        ocrHintInit(_hintPtr, OCR_HINT_EDT_T);
+    }
+    s64 _priorityWeight = {{g.priorityFn(stepfun.collName, util.g_ctx_var()~"->_affinityCount")}};
+    ocrSetHintValue(_hintPtr, OCR_HINT_EDT_PRIORITY, _priorityWeight);
+    {%- else -%}
+    ocrHint_t *const _hintPtr = _cncCurrentEdtAffinityHint(&_hint);
+    {%- endif %}
     ocrEdtCreate(&_stepGuid, {{util.g_ctx_var()}}->_steps.{{stepfun.collName}},
         {% if paramTag -%}
         /*paramc=*/{{(stepfun.tag|count)}}, /*paramv=*/_tag,
@@ -91,7 +102,7 @@ static void cncPrescribeInternal_{{stepfun.collName}}({{
         {% endif -%}
         /*depc=*/_depc, /*depv=*/NULL,
         /*properties=*/EDT_PROP_NONE,
-        /*affinity=*/_cncCurrentAffinity(), /*outEvent=*/NULL);
+        /*hint=*/_hintPtr, /*outEvent=*/NULL);
 
     s32 _edtSlot = 0; MAYBE_UNUSED(_edtSlot);
     ocrAddDependence({{util.g_ctx_var()}}->_guids.self, _stepGuid, _edtSlot++, DB_MODE_RO);
@@ -185,11 +196,13 @@ void cncPrescribe_{{stepfun.collName}}({{
         // XXX - should just create this template along with the step function template
         ocrGuid_t edtGuid, templGuid;
         ocrEdtTemplateCreate(&templGuid, _cncRemotePrescribe_{{stepfun.collName}}, _argCount, _depCount);
+        ocrHint_t _hint;
         ocrEdtCreate(&edtGuid, templGuid,
                 /*paramc=*/_argCount, /*paramv=*/_args,
                 /*depc=*/_depCount, /*depv=*/_deps,
                 /*properties=*/EDT_PROP_NONE,
-                /*affinity=*/_affinity, /*outEvent=*/NULL);
+                /*hint=*/_cncEdtAffinityHint(&_hint, _affinity),
+                /*outEvent=*/NULL);
         ocrEdtTemplateDestroy(templGuid);
         return;
     }
