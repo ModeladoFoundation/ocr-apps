40800400b6d06c823d1e25362d788842d3f8031c
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-07-05 15:04:03 -0500

    Add test for test returning null handle

---
+++ test/taskCreateReturnNull/taskCreateReturnNull.cpp
@@ -0,0 +1,14 @@
+#include <ocxxr.hpp>
+
+ocxxr::NullHandle ChildTask() {
+    PRINTF("Child task ran!\n");
+    PRINTF("Shutting down...\n");
+    ocxxr::Shutdown();
+    return ocxxr::NullHandle();
+}
+
+void ocxxr::Main(ocxxr::AcquiredDatablock<ocxxr::MainTaskArgs>) {
+    PRINTF("Creating child task\n");
+    auto task_template = OCXXR_TEMPLATE_FOR(ChildTask);
+    task_template.CreateTask();
+}
