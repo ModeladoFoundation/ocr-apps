d50b7cd0e65f0699a8535008014c1b65529cdf43
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-07-05 21:13:36 -0500

    Remove using directive from header

--- include/ocxxr.hpp
+++ include/ocxxr.hpp
@@ -35,10 +35,6 @@ using EnableIfBaseOf =
 inline void OK(u8 status) { ASSERT(status == 0); }
 }
 
-// NOTE: I add more declarations to the internal namespace later,
-// but this directive brings them all into the current scope.
-using namespace internal;
-
 //! Abstract base class for all OCR objects with GUIDs.
 class ObjectHandle {
  public:
@@ -69,7 +65,7 @@ class ObjectHandle {
     const ocrGuid_t guid_;
 };
 
-static_assert(IsLegalHandle<ObjectHandle>::value,
+static_assert(internal::IsLegalHandle<ObjectHandle>::value,
               "ObjectHandle must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -81,7 +77,7 @@ class DataHandle : public ObjectHandle {
     explicit DataHandle(ocrGuid_t guid) : ObjectHandle(guid) {}
 };
 
-static_assert(IsLegalHandle<DataHandle<int>>::value,
+static_assert(internal::IsLegalHandle<DataHandle<int>>::value,
               "NullHandle must be castable to/from ocrGuid_t.");
 
 void Shutdown() { ocrShutdown(); }
@@ -115,7 +111,7 @@ class Datablock : public DataHandle<T> {
     explicit Datablock(T **data_ptr, u64 count = 1)
             : DataHandle<T>(Init(data_ptr, sizeof(T) * count, true, nullptr)) {}
 
-    void Destroy() const { OK(ocrDbDestroy(this->guid())); }
+    void Destroy() const { internal::OK(ocrDbDestroy(this->guid())); }
 
  protected:
     explicit Datablock(ocrGuid_t guid) : DataHandle<T>(guid) {}
@@ -134,12 +130,13 @@ class Datablock : public DataHandle<T> {
         // E.g., "const ocrHint_t *hint" in ocrDbCreate.
         ocrHint_t *raw_hint = const_cast<ocrHint_t *>(hint->internal());
         void **raw_data_ptr = reinterpret_cast<void **>(data_ptr);
-        OK(ocrDbCreate(&guid, raw_data_ptr, bytes, flags, raw_hint, NO_ALLOC));
+        internal::OK(ocrDbCreate(&guid, raw_data_ptr, bytes, flags, raw_hint,
+                                 NO_ALLOC));
         return guid;
     }
 };
 
-static_assert(IsLegalHandle<Datablock<int>>::value,
+static_assert(internal::IsLegalHandle<Datablock<int>>::value,
               "Datablock must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -158,7 +155,7 @@ class AcquiredDatablock : public Datablock<T> {
             : Datablock<T>(NULL_GUID), data_(nullptr) {}
 
     template <typename U = T>
-    EnableIfNotVoid<U, U> &data() const {
+    internal::EnableIfNotVoid<U, U> &data() const {
         // The template type U is only here to get enable_if to work.
         static_assert(std::is_same<T, U>::value, "Template types must match.");
         return *data_;
@@ -166,7 +163,7 @@ class AcquiredDatablock : public Datablock<T> {
 
     T *data_ptr() const { return data_; }
 
-    void Release() const { OK(ocrDbRelease(this->guid())); }
+    void Release() const { internal::OK(ocrDbRelease(this->guid())); }
 
  private:
     AcquiredDatablock(T *tmp, u64 count)
@@ -180,12 +177,14 @@ class Event : public DataHandle<T> {
  public:
     explicit Event(ocrEventTypes_t type) : DataHandle<T>(Init(type)) {}
 
-    void Destroy() const { OK(ocrEventDestroy(this->guid())); }
+    void Destroy() const { internal::OK(ocrEventDestroy(this->guid())); }
 
-    void Satisfy() const { OK(ocrEventSatisfy(this->guid(), NULL_GUID)); }
+    void Satisfy() const {
+        internal::OK(ocrEventSatisfy(this->guid(), NULL_GUID));
+    }
 
     void Satisfy(DataHandle<T> data) const {
-        OK(ocrEventSatisfy(this->guid(), data.guid()));
+        internal::OK(ocrEventSatisfy(this->guid(), data.guid()));
     }
 
  private:
@@ -193,12 +192,12 @@ class Event : public DataHandle<T> {
         ocrGuid_t guid;
         static constexpr bool kIsVoid = std::is_same<void, T>::value;
         constexpr u16 flags = kIsVoid ? EVT_PROP_NONE : EVT_PROP_TAKES_ARG;
-        OK(ocrEventCreate(&guid, type, flags));
+        internal::OK(ocrEventCreate(&guid, type, flags));
         return guid;
     }
 };
 
-static_assert(IsLegalHandle<Event<int>>::value,
+static_assert(internal::IsLegalHandle<Event<int>>::value,
               "Event must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -207,7 +206,7 @@ class OnceEvent : public Event<T> {
     explicit OnceEvent() : Event<T>(OCR_EVENT_ONCE_T) {}
 };
 
-static_assert(IsLegalHandle<OnceEvent<int>>::value,
+static_assert(internal::IsLegalHandle<OnceEvent<int>>::value,
               "OnceEvent must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -216,7 +215,7 @@ class IdempotentEvent : public Event<T> {
     explicit IdempotentEvent() : Event<T>(OCR_EVENT_IDEM_T) {}
 };
 
-static_assert(IsLegalHandle<IdempotentEvent<int>>::value,
+static_assert(internal::IsLegalHandle<IdempotentEvent<int>>::value,
               "IdempotentEvent must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -225,7 +224,7 @@ class StickyEvent : public Event<T> {
     explicit StickyEvent() : Event<T>(OCR_EVENT_STICKY_T) {}
 };
 
-static_assert(IsLegalHandle<StickyEvent<int>>::value,
+static_assert(internal::IsLegalHandle<StickyEvent<int>>::value,
               "StickyEvent must be castable to/from ocrGuid_t.");
 
 template <typename T>
@@ -237,7 +236,7 @@ class LatchEvent : public Event<T> {
     // TODO - Down
 };
 
-static_assert(IsLegalHandle<LatchEvent<int>>::value,
+static_assert(internal::IsLegalHandle<LatchEvent<int>>::value,
               "LatchEvent must be castable to/from ocrGuid_t.");
 
 class NullHandle : public ObjectHandle {
@@ -249,7 +248,7 @@ class NullHandle : public ObjectHandle {
     const operator T() const {
         static_assert(std::is_base_of<ObjectHandle, T>::value,
                       "Only use NullHandle for ObjectHandle types.");
-        static_assert(IsLegalHandle<T>::value,
+        static_assert(internal::IsLegalHandle<T>::value,
                       "Only use NullHandle for simple handle types.");
         return *reinterpret_cast<const T *>(this);
     }
@@ -260,7 +259,7 @@ class NullHandle : public ObjectHandle {
     }
 };
 
-static_assert(IsLegalHandle<NullHandle>::value,
+static_assert(internal::IsLegalHandle<NullHandle>::value,
               "NullHandle must be castable to/from ocrGuid_t.");
 
 namespace internal {
@@ -287,7 +286,8 @@ struct DependenceArgFor;
 
 template <template <typename> class T, typename U>
 struct DependenceArgFor<T<U>> {
-    typedef EnableIfBaseOf<DataHandle<U>, T<U>, AcquiredDatablock<U>> Type;
+    typedef internal::EnableIfBaseOf<DataHandle<U>, T<U>, AcquiredDatablock<U>>
+            Type;
 };
 template <>
 struct DependenceArgFor<NullHandle> {
@@ -335,26 +335,26 @@ class TaskImplementation<F, user_fn, R(Args...)> {
     static_assert(std::is_same<F, R(Args...)>::value,
                   "Task function must have a consistent type.");
 
-    static ocrGuid_t internal_fn(u32 paramc, u64 /*paramv*/[], u32 depc,
-                                 ocrEdtDep_t depv[]) {
+    static ocrGuid_t InternalFn(u32 paramc, u64 /*paramv*/[], u32 depc,
+                                ocrEdtDep_t depv[]) {
         ASSERT(paramc == 0);
         ASSERT(depc == sizeof...(Args));
-        return launch(depv, std::index_sequence_for<Args...>{});
+        return Launch(depv, std::index_sequence_for<Args...>{});
     }
 
  private:
     // XXX - using index_sequence requires C++14,
     // but we could roll our own if we really want C++11 compatibility.
     template <typename U = R, size_t... Indices>
-    static EnableIfNotVoid<U, ocrGuid_t> launch(
+    static internal::EnableIfNotVoid<U, ocrGuid_t> Launch(
             ocrEdtDep_t deps[], std::index_sequence<Indices...>) {
         // This calls the AcquiredDatablock(ocrEdtDep_t) constructor
         return user_fn((Args{deps[Indices]})...).guid();
     }
 
     template <typename U = R, size_t... Indices>
-    static EnableIfVoid<U, ocrGuid_t> launch(ocrEdtDep_t deps[],
-                                             std::index_sequence<Indices...>) {
+    static internal::EnableIfVoid<U, ocrGuid_t> Launch(
+            ocrEdtDep_t deps[], std::index_sequence<Indices...>) {
         // This calls the AcquiredDatablock(ocrEdtDep_t) constructor
         user_fn((Args{deps[Indices]})...);
         return NULL_GUID;
@@ -368,12 +368,12 @@ class TaskTemplateBase : public ObjectHandle {
  public:
     ~TaskTemplateBase() = default;
 
-    void Destroy() const { OK(ocrEdtTemplateDestroy(this->guid())); }
+    void Destroy() const { internal::OK(ocrEdtTemplateDestroy(this->guid())); }
 
  protected:
     explicit TaskTemplateBase(ocrGuid_t guid) : ObjectHandle(guid) {}
 
-    typedef typename FnInfo<F>::ResultType R;
+    typedef typename internal::FnInfo<F>::ResultType R;
     static_assert(std::is_same<void, R>::value ||
                           std::is_base_of<ObjectHandle, R>::value,
                   "User's task function must return an OCR object type.");
@@ -387,12 +387,12 @@ class Task : public ObjectHandle {
     Task(TaskTemplateBase<F> task_template, Deps... deps)
             : ObjectHandle(Init(task_template, deps...)) {}
 
-    void Destroy() const { OK(ocrEdtDestroy(this->guid())); }
+    void Destroy() const { internal::OK(ocrEdtDestroy(this->guid())); }
 
     // TODO - AddDependence
 
  private:
-    static constexpr u64 depc = FnInfo<F>::kArgCount;
+    static constexpr u64 depc = internal::FnInfo<F>::kArgCount;
 
     static ocrGuid_t Init(TaskTemplateBase<F> task_template, Deps... deps) {
         ocrGuid_t guid;
@@ -405,21 +405,22 @@ class Task : public ObjectHandle {
 
     // Note: this assertion won't ever fail because another insertion
     // in the internal checks will always fail first (or none at all).
-    static_assert(TaskArgCountCheck<sizeof...(Deps), depc>::value &&
-                          TaskArgsMatchDeps<0, F, Deps...>::value,
+    static_assert(internal::TaskArgCountCheck<sizeof...(Deps), depc>::value &&
+                          internal::TaskArgsMatchDeps<0, F, Deps...>::value,
                   "Check for args/paramters mismatch.");
 };
 
 template <typename F>
 class TaskTemplate : public TaskTemplateBase<F> {
  public:
-    typedef typename FnInfo<F>::ResultType ResultType;
+    typedef typename internal::FnInfo<F>::ResultType ResultType;
 
     template <F *user_fn>
     static TaskTemplate<F> Create() {
         ocrGuid_t guid;
-        ocrEdt_t internal_fn = TaskImplementation<F, user_fn, F>::internal_fn;
-        constexpr u64 depc = FnInfo<F>::kArgCount;
+        ocrEdt_t internal_fn =
+                internal::TaskImplementation<F, user_fn, F>::InternalFn;
+        constexpr u64 depc = internal::FnInfo<F>::kArgCount;
         ocrEdtTemplateCreate(&guid, internal_fn, 0, depc);
         return TaskTemplate<F>(guid);
     }
@@ -447,15 +448,19 @@ typedef NullHandle DummyTaskFnType(AcquiredDatablock<int>,
 
 typedef Task<DummyTaskFnType, Datablock<int>, Datablock<double>> DummyTaskType;
 
+typedef TaskTemplateBase<DummyTaskFnType> DummyTemplateBaseType;
+
+typedef TaskTemplate<DummyTaskFnType> DummyTemplateType;
+
 }  // namespace internal
 
-static_assert(IsLegalHandle<TaskTemplateBase<DummyTaskFnType>>::value,
+static_assert(internal::IsLegalHandle<internal::DummyTemplateBaseType>::value,
               "TaskTemplateBase must be castable to/from ocrGuid_t.");
 
-static_assert(IsLegalHandle<DummyTaskType>::value,
+static_assert(internal::IsLegalHandle<internal::DummyTaskType>::value,
               "Task must be castable to/from ocrGuid_t.");
 
-static_assert(IsLegalHandle<TaskTemplate<DummyTaskFnType>>::value,
+static_assert(internal::IsLegalHandle<internal::DummyTemplateType>::value,
               "TaskTemplate must be castable to/from ocrGuid_t.");
 
 // prototype for user's main function
