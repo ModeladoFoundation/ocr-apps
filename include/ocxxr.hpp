7441d2367af71b7e306d3462fdb3a8125ef79058
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-07-08 12:14:33 -0500

    Add EDT and DB hint types

--- include/ocxxr.hpp
+++ include/ocxxr.hpp
@@ -91,10 +91,52 @@ void Abort(u8 error_code) { ocrAbort(error_code); }
 // TODO - add Hint subclasses: TaskHint, DatablockHint
 class Hint {
  public:
+    Hint(ocrHintType_t hint_type) {
+        internal::OK(ocrHintInit(&hint_, hint_type));
+    }
+
+    Hint &Set(ocrHintProp_t prop, u64 value) {
+        internal::OK(ocrSetHintValue(&hint_, prop, value));
+        return *this;
+    }
+
+    Hint &Unset(ocrHintProp_t prop) {
+        internal::OK(ocrUnsetHintValue(&hint_, prop));
+        return *this;
+    }
+
+    u64 Get(ocrHintProp_t prop) {
+        u64 value;
+        internal::OK(ocrGetHintValue(&hint_, prop, &value));
+        return value;
+    }
+
+    Hint &AttachTo(ObjectHandle object) {
+        internal::OK(ocrSetHint(object.guid(), &hint_));
+        return *this;
+    }
+
+    Hint &ReadFrom(ObjectHandle object) {
+        internal::OK(ocrGetHint(object.guid(), &hint_));
+        return *this;
+    }
+
     const ocrHint_t *internal() const { return &hint_; }
+
+    ocrHint_t *internal() { return &hint_; }
 
  private:
     ocrHint_t hint_;
+};
+
+class TaskHint : public Hint {
+ public:
+    TaskHint() : Hint(OCR_HINT_EDT_T) {}
+};
+
+class DatablockHint : public Hint {
+ public:
+    DatablockHint() : Hint(OCR_HINT_DB_T) {}
 };
 
 //! Datablocks
