4c3cbe35e9580a27c5397191e0852008159ce84f
Chandra S Martha <chandra.martha@intel.com>
2016-07-07 18:40:17 -0700

    CoMD: A better OCR implementation
    * Still has bugs; Runs on a single node
    * Distributed fails occasionally with NaNs
    
    Change-Id: I3e3b30d2c9b714933921ca581585183dc400a5b0

---
+++ apps/CoMD/refactored/ocr/intel-chandra/comd.h
@@ -0,0 +1,319 @@
+#ifndef COMD_H
+#define COMD_H
+
+#include <string.h>
+#include <math.h>
+#include <ocr.h>
+
+#include "constants.h"
+
+void getPartitionID(s64 i, s64 lb_g, s64 ub_g, s64 R, s64* id);
+void splitDimension(s64 Num_procs, s64* Num_procsx, s64* Num_procsy, s64* Num_procsz);
+
+typedef double real_t;
+typedef real_t real3_t[3];
+
+typedef struct
+{
+  real_t sigma;
+  real_t epsilon;
+} lj_t;
+
+typedef struct
+{
+  u32 n;
+  real_t x0;
+  real_t xn;
+  real_t inv_dx;
+  ocrGuid_t values;
+} interpolator_t;
+
+typedef struct
+{
+  interpolator_t phi;
+  interpolator_t rho;
+  interpolator_t f;
+} eam_t;
+
+typedef struct
+{
+  real_t mass[MAXSPECIES];
+  real_t inv_mass[MAXSPECIES];
+  real_t inv_mass_2[MAXSPECIES];
+  real_t inv_mass_dt[MAXSPECIES];
+} mass_t;
+
+typedef struct
+{
+  u8 lattice_type;
+  u8 potential;
+
+  real_t cutoff;
+  real_t massFloat;
+  ocrGuid_t mass;
+  real_t lat;
+  u32 atomic_no;
+
+  void (*print)(void* p);
+
+  ocrGuid_t (*force_edt)(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]);
+  ocrGuid_t (*forcevel_edt)(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]);
+
+  union {
+    lj_t lj;
+    eam_t eam;
+  };
+} potential_t;
+
+typedef struct
+{
+  u32 nAtoms;
+  u8 nmask;
+
+  u32 gid[MAXATOMS];
+  u32 iSpecies[MAXATOMS];
+  //u8 s[MAXATOMS];
+  real3_t r[MAXATOMS];
+  real3_t p[MAXATOMS];
+  real3_t f[MAXATOMS];
+  real_t u[MAXATOMS];
+
+  ocrGuid_t linkCellGuid;
+  ocrGuid_t nextpf;
+} atomData_t;
+
+typedef struct
+{
+  u32 coordinates[3];
+  real3_t min;
+  real3_t max;
+  u32 max_occupancy;
+  ocrGuid_t nbr_atomDataGuids[27]; //TODO: DO I need 26 or 27?
+  ocrGuid_t nbr_linkCellGuids[27];
+} linkCellH_t;
+
+typedef struct
+{
+    atomData_t* atoms;
+    linkCellH_t* boxes;
+    mass_t* species;
+} SimFlat_t;
+
+
+//typedef struct
+//{
+//  u32 atoms;
+//  u8 nmask;
+//  real3_t r[MAXATOMS];
+//  real3_t p[MAXATOMS];
+//  real3_t f[MAXATOMS];
+//  real_t u[MAXATOMS];
+//  u8 s[MAXATOMS];
+//  ocrGuid_t linkCellGuid;
+//  ocrGuid_t nextpf;
+//} atomData_t;
+//
+//typedef struct
+//{
+//  u32 coordinates[3];
+//  real3_t min;
+//  real3_t max;
+//  u32 max_occupancy;
+//  u32 gid[MAXATOMS];
+//  ocrGuid_t nbr_atomDataGuids[26];
+//  ocrGuid_t nbr_linkCellGuids[26];
+//} linkCellH_t;
+
+//typedef struct
+//{
+//  real3_t domain;
+//  u32 grid[3];
+//  u32 b_num;
+//  real3_t b_size;
+//  real3_t inv_b_size;
+//  ocrGuid_t DBK_linkCellGuidsH;
+//  ocrGuid_t DBK_atomDataGuidsH;
+//} boxDataStH_t;
+
+typedef struct
+{
+    //localDataSt_t localDataSt;
+    ocrGuid_t DBK_linkCell;
+    ocrGuid_t DBK_atomData;
+    ocrGuid_t EVT_finalize;
+} privateDataSt_t;
+
+typedef struct
+{
+  real3_t value;
+  ocrGuid_t OEVT_reduction;
+  ocrGuid_t sim;
+} reductionH_t;
+
+typedef struct
+{
+  u32 step;
+  u32 steps;
+  u32 period;
+  real_t dt;
+
+  potential_t pot;
+
+  //boxDataStH_t boxDataStH;
+
+  real_t e_potential;
+  real_t e_kinetic;
+  u32 atoms;
+
+  double energy0;
+  u32 atoms0;
+
+  ocrGuid_t reductionH_g;
+
+  ocrGuid_t EVT_MAP_position, EVT_MAP_redistribute;
+  ocrGuid_t EVT_MAP_reduction;
+
+  ocrGuid_t OEVT_finalize;
+
+} simulationH_t;
+
+//Param structs
+typedef struct
+{
+    ocrGuid_t EDT_finalize;
+} PRM_period_edt_t;
+
+typedef struct
+{
+    u32 grid[3];
+    u32 lattice[3];
+    real_t delta, temperature;
+} PRM_EDT_init_fork_t;
+
+typedef struct
+{
+    u32 coords[3];
+    u32 lattice[3];
+    real_t delta, temperature;
+    ocrGuid_t vleaf, tleaf, uleaf;
+} PRM_FNC_init_t;
+
+typedef struct
+{
+    ocrGuid_t tleaf;
+    ocrGuid_t EDT_sched;
+} PRM_vcm_edt_t;
+
+typedef struct
+{
+    real_t dt;
+    ocrGuid_t uleaf;
+} PRM_ukvel_edt_t;
+
+typedef struct
+{
+    lj_t lj;
+    real_t cutoff;
+    real_t domain[3];
+} computeForceEdtParamv_t;
+
+typedef struct
+{
+    u32 n;
+    real_t temperature, epsilon;
+    ocrGuid_t guid;
+} PRM_red_t;
+
+typedef struct
+{
+    u32 n;
+    real_t epsilon;
+} PRM_ured_edt_t;
+
+typedef struct
+{
+    u32 n;
+    real_t temperature;
+    ocrGuid_t guid;
+} PRM_tred_edt_t;
+
+typedef struct
+{
+    u32 n;
+    ocrGuid_t guid;
+} PRM_vred_edt_t;
+
+typedef struct
+{
+    real_t ds, dt;
+    ocrGuid_t EDT_exchange, DBK_linkCellH;
+} PRM_position_edt_t;
+
+typedef struct
+{
+    real_t ds, sigma, epsilon, cutoff;
+    real_t domain[3];
+    ocrGuid_t leaves_g;
+} PRM_forcevel_edt_t;
+
+typedef struct
+{
+    real_t dt;
+    ocrGuid_t leaves_g;
+} PRM_veluk_edt_t;
+
+
+static inline u32 neighbor_id(u8 n, u32* coordinates, u32* grid)
+{
+  u32 c[3] = {coordinates[0], coordinates[1], coordinates[2]};
+
+  if(n>12)
+    ++n;
+
+  if(n<9) {
+    if(c[2]) --c[2];
+    else c[2] = grid[2]-1;
+  } else if(n>17) {
+    if(c[2]+1==grid[2]) c[2]=0;
+    else ++c[2];
+  }
+  n%=9;
+  if(n<3) {
+    if(c[1]) --c[1];
+    else c[1] = grid[1]-1;
+  } else if(n>5) {
+    if(c[1]+1==grid[1]) c[1]=0;
+    else ++c[1];
+  }
+  n%=3;
+  c[0] = (c[0]+grid[0]-1+n)%grid[0];
+
+  return (c[0]*grid[1]+c[1])*grid[2]+c[2];
+}
+
+static inline int globalRankFromCoords( int id_x, int id_y, int id_z, int NR_X, int NR_Y, int NR_Z )
+{
+    return NR_X*NR_Y*id_z + NR_X*id_y + id_x;
+}
+
+static inline int getPoliyDomainID( int b, u32* grid, int PD_X, int PD_Y, int PD_Z )
+{
+    int id_x = b%grid[0];
+    int id_y = (b/grid[0])%grid[1];
+    int id_z = (b/grid[0])/grid[1];
+
+    s64 pd_x; getPartitionID(id_x, 0, grid[0]-1, PD_X, &pd_x);
+    s64 pd_y; getPartitionID(id_y, 0, grid[1]-1, PD_Y, &pd_y);
+    s64 pd_z; getPartitionID(id_z, 0, grid[2]-1, PD_Z, &pd_z);
+
+    //Each linkcell, with id=b, is mapped to a PD. The mapping is similar to how the link cells map to
+    //MPI ranks. In other words, all the PDs are arranged as a 3-D grid.
+    //And, a 3-D subgrid of linkcells is mapped to a PD preserving "locality" within a PD.
+    //
+    int pd = globalRankFromCoords(pd_x, pd_y, pd_z, PD_X, PD_Y, PD_Z);
+    //PRINTF("%d linkCell %d %d %d, policy domain %d: %d %d %d\n", b, id_x, id_y, id_z, pd, PD_X, PD_Y, PD_Z);
+
+    return pd;
+}
+
+#endif
---
+++ apps/CoMD/refactored/ocr/intel-chandra/lj.c
@@ -0,0 +1,240 @@
+#include <string.h>
+#include <ocr.h>
+
+#include "extensions/ocr-affinity.h"
+
+#include "comd.h"
+#include "constants.h"
+#include "potentials.h"
+
+ocrGuid_t ljforcevel_edt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[])
+{
+    return NULL_GUID;
+}
+
+//extern ocrGuid_t ljforce_edt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]);
+
+#if 1
+void ljforce(atomData_t* PTR_atomData, atomData_t** nPTR_atomData, real_t sigma, real_t epsilon, real_t cut, real_t* shift)
+{
+  real_t pot = 0;
+  real_t cut2 = cut*cut;
+  real_t s6 = sigma*sigma*sigma*sigma*sigma*sigma;
+  real_t cut6 = s6 /(cut2*cut2*cut2);
+  real_t pshift = POT_SHIFT*cut6*(cut6-1.0);
+
+  u8 aa,bb;
+  for(aa = 0; aa < PTR_atomData->nAtoms; ++aa)
+    for(bb = aa+1; bb < PTR_atomData->nAtoms; ++bb) {
+      real_t r2; real3_t dr;
+      dr[0] = PTR_atomData->r[aa][0]-PTR_atomData->r[bb][0];
+      dr[1] = PTR_atomData->r[aa][1]-PTR_atomData->r[bb][1];
+      dr[2] = PTR_atomData->r[aa][2]-PTR_atomData->r[bb][2];
+      r2=dr[0]*dr[0]+dr[1]*dr[1]+dr[2]*dr[2];
+
+      if(r2 > cut2) continue;
+      r2 = 1.0/r2;
+
+      real_t r6 = s6 * (r2*r2*r2);
+      real_t energy = (r6*(r6-1.0)-pshift)*0.5;
+      PTR_atomData->u[aa]+=energy;
+      PTR_atomData->u[bb]+=energy;
+
+      real_t fr = -epsilon*r6*r2*(12.0*r6-6.0);
+      PTR_atomData->f[aa][0]-=dr[0]*fr;
+      PTR_atomData->f[aa][1]-=dr[1]*fr;
+      PTR_atomData->f[aa][2]-=dr[2]*fr;
+      PTR_atomData->f[bb][0]+=dr[0]*fr;
+      PTR_atomData->f[bb][1]+=dr[1]*fr;
+      PTR_atomData->f[bb][2]+=dr[2]*fr;
+    }
+
+  u8 n;
+  for(n = 0; n <= 26; ++n) {
+  if(n==13) continue; //computed above
+    atomData_t* bPTR_atomData = nPTR_atomData[n];
+    for(aa = 0; aa < PTR_atomData->nAtoms; ++aa)
+      for(bb = 0; bb < bPTR_atomData->nAtoms; ++bb) {
+        real_t r2; real3_t dr;
+        dr[0] = PTR_atomData->r[aa][0]-(bPTR_atomData->r[bb][0]+FSHIFT_X(PTR_atomData->nmask,n));
+        dr[1] = PTR_atomData->r[aa][1]-(bPTR_atomData->r[bb][1]+FSHIFT_Y(PTR_atomData->nmask,n));
+        dr[2] = PTR_atomData->r[aa][2]-(bPTR_atomData->r[bb][2]+FSHIFT_Z(PTR_atomData->nmask,n));
+        r2=dr[0]*dr[0]+dr[1]*dr[1]+dr[2]*dr[2];
+        //PRINTF("gid %d-%d %18.12f %18.12f %18.12f %8.6f %8.6f %8.6f %8.6f %8.6f %8.6f s: %18.12f %18.12f %18.12f\n", PTR_atomData->gid[aa], bPTR_atomData->gid[bb], dr[0], dr[1], dr[2], PTR_atomData->r[aa][0], PTR_atomData->r[aa][1], PTR_atomData->r[aa][2],
+        //bPTR_atomData->r[bb][0], bPTR_atomData->r[bb][1], bPTR_atomData->r[bb][2],
+        //FSHIFT_X(PTR_atomData->nmask,n),
+        //FSHIFT_Y(PTR_atomData->nmask,n),
+        //FSHIFT_Z(PTR_atomData->nmask,n)
+        //);
+
+        if(r2 > cut2) continue;
+        r2 = 1.0/r2;
+
+        real_t r6 = s6 * (r2*r2*r2);
+        real_t energy = (r6*(r6-1.0)-pshift)*0.5;
+        PTR_atomData->u[aa]+=energy;
+
+        real_t fr = -epsilon*r6*r2*(12.0*r6-6.0);
+        PTR_atomData->f[aa][0]-=dr[0]*fr;
+        PTR_atomData->f[aa][1]-=dr[1]*fr;
+        PTR_atomData->f[aa][2]-=dr[2]*fr;
+      }
+  }
+
+}
+
+//params: sigma, epsilon, cutoff, shiftx, shifty, shiftz
+//depv: DBK_atomDataH0, .., DBK_atomDataH26
+ocrGuid_t ljforce_edt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[])
+{
+  //PRINTF("ljforce_edt\n");
+  atomData_t* PTR_atomData = (atomData_t*)depv[13].ptr;
+#ifndef TG_ARCH
+  memset(PTR_atomData->f,0,sizeof(real3_t)*PTR_atomData->nAtoms);
+  memset(PTR_atomData->u,0,sizeof(real_t)*PTR_atomData->nAtoms);
+#else
+  u32 m;
+  for(m=0; m<sizeof(real3_t)*PTR_atomData->nAtoms; ++m) ((char*)PTR_atomData->f)[m]='\0';
+  for(m=0; m<sizeof(real_t)*PTR_atomData->nAtoms; ++m) ((char*)PTR_atomData->u)[m]='\0';
+#endif
+
+  computeForceEdtParamv_t* PTR_PRM_force_edt = (computeForceEdtParamv_t*) paramv;
+  real_t sigma = PTR_PRM_force_edt->lj.sigma;
+  real_t epsilon = PTR_PRM_force_edt->lj.epsilon;
+  real_t cut = PTR_PRM_force_edt->cutoff;
+  real_t* shift = PTR_PRM_force_edt->domain;
+
+  atomData_t* nPTR_atomData[27];
+  u8 n, k=0;
+  for(n = 0; n <= 26; ++n)
+  {
+    //if(n==13) continue;
+    nPTR_atomData[k++] = (atomData_t*)depv[n].ptr;
+    //PRINTF("n %d %p\n", n, nPTR_atomData[k-1]);
+  }
+
+  ljforce(PTR_atomData, nPTR_atomData, sigma, epsilon, cut, shift);
+
+  return NULL_GUID; //depv[0].guid;
+}
+
+#endif
+
+#if 0
+
+//params: count, leaf, sigma, epsilon, cutoff, shiftx, shifty, shiftz
+//depv: DBK_atomDataH0, .., DBK_atomDataH26, sim, nextff, signals0, .., signals26
+ocrGuid_t ljforcevel_edt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[])
+{
+    ocrGuid_t PDaffinityGuid = NULL_GUID;
+    ocrHint_t HNT_edt;
+    ocrHintInit( &HNT_edt, OCR_HINT_EDT_T );
+#ifdef ENABLE_EXTENSION_AFFINITY
+    ocrAffinityGetCurrent(&PDaffinityGuid);
+    ocrSetHintValue( &HNT_edt, OCR_HINT_EDT_AFFINITY, ocrAffinityToHintValue(PDaffinityGuid) );
+#endif
+
+  ocrGuid_t DBK_atomDataH = depv[0].guid;
+
+  atomData_t* PTR_atomData = (atomData_t*)depv[0].ptr;
+#ifndef TG_ARCH
+  memset(PTR_atomData->f,0,sizeof(real3_t)*PTR_atomData->nAtoms);
+  memset(PTR_atomData->u,0,sizeof(real_t)*PTR_atomData->nAtoms);
+#else
+  u32 m;
+  for(m=0; m<sizeof(real3_t)*PTR_atomData->nAtoms; ++m) ((char*)PTR_atomData->f)[m]='\0';
+  for(m=0; m<sizeof(real_t)*PTR_atomData->nAtoms; ++m) ((char*)PTR_atomData->u)[m]='\0';
+#endif
+
+  PRM_forcevel_edt_t* PTR_PRM_forcevel_edt = (PRM_forcevel_edt_t*) paramv;
+  real_t ds = PTR_PRM_forcevel_edt->ds;
+  real_t sigma = PTR_PRM_forcevel_edt->sigma;
+  real_t epsilon = PTR_PRM_forcevel_edt->epsilon;
+  real_t cut = PTR_PRM_forcevel_edt->cutoff;
+  real_t* shift = PTR_PRM_forcevel_edt->domain;
+
+  atomData_t* nPTR_atomData[26];
+  u8 n;
+  for(n = 0; n < 26; ++n)
+    nPTR_atomData[n] = (atomData_t*)depv[n+1].ptr;
+
+  ljforce(PTR_atomData, nPTR_atomData, sigma, epsilon, cut, shift);
+
+  simulationH_t* sim = (simulationH_t*)depv[27].ptr;
+
+  ocrGuid_t tmp, tmpg;
+  if(--ds) {
+    PTR_PRM_forcevel_edt->ds = ds;
+    velocity(PTR_atomData, sim->dt);
+    ocrEdtTemplateCreate(&tmp, ljforcevel_edt, sizeof(PRM_forcevel_edt_t)/sizeof(u64), 56);
+    ocrEdtCreate(&tmpg, tmp, EDT_PARAM_DEF, (u64*)PTR_PRM_forcevel_edt, 56, NULL, EDT_PROP_NONE, PICK_1_1(&HNT_edt,PDaffinityGuid), (ocrGuid_t*)depv[28].ptr);
+    ocrDbRelease(depv[28].guid);
+    ocrEdtTemplateDestroy(tmp);
+    ocrAddDependence(PTR_atomData->nextpf, tmpg, 29, DB_MODE_NULL);
+    ocrDbRelease(DBK_atomDataH);
+    ocrAddDependence(DBK_atomDataH, tmpg, 0, DB_MODE_RW);
+    for(n=1; n < 28; ++n)
+      ocrAddDependence(depv[n].guid, tmpg, n, DB_MODE_RO);
+    ocrAddDependence(depv[n].guid, tmpg, n, DB_MODE_RW);
+    for(n=30; n < 56; ++n)
+      ocrAddDependence(nPTR_atomData[n-30]->nextpf, tmpg, n, DB_MODE_NULL);
+    return depv[28].guid;
+  }
+  else {
+    PRM_veluk_edt_t PRM_veluk_edt;
+    PRM_veluk_edt.dt = 0.5*sim->dt;
+    PRM_veluk_edt.leaves_g = PTR_PRM_forcevel_edt->leaves_g;
+    ocrEdtTemplateCreate(&tmp, veluk_edt, sizeof(PRM_veluk_edt_t)/sizeof(u64), 2);
+    ocrEdtCreate(&tmpg, tmp, EDT_PARAM_DEF, (u64*)&PRM_veluk_edt, 2, NULL, EDT_PROP_NONE, PICK_1_1(&HNT_edt,PDaffinityGuid), NULL);
+    ocrEdtTemplateDestroy(tmp);
+    ocrDbRelease(DBK_atomDataH);
+    ocrAddDependence(DBK_atomDataH, tmpg, 0, DB_MODE_RW);
+    ocrAddDependence(sim->pot.mass, tmpg, 1, DB_MODE_RO);
+    ocrDbDestroy(depv[28].guid);
+  }
+
+  return NULL_GUID;
+}
+
+#endif
+
+void print_lj(void* po)
+{
+  potential_t* p = (potential_t*)po;
+  PRINTF("  Potential type   : Lennard-Jones\n");
+  PRINTF("  Species name     : Cu\n");
+  PRINTF("  Atomic number    : %u\n", p->atomic_no);
+  PRINTF("  Mass             : %.3f amu\n", p->massFloat / amu2internal_mass);
+  PRINTF("  Lattice Type     : FCC\n");
+  PRINTF("  Lattice spacing  : %.3f Angstroms\n", p->lat);
+  PRINTF("  Cutoff           : %.3f Angstroms\n", p->cutoff);
+  PRINTF("  Epsilon          : %.3f eV\n", p->lj.epsilon*.25);
+  PRINTF("  Sigma            : %.3f Angstroms\n", p->lj.sigma);
+}
+
+void init_lj(potential_t* p, real_t dt)
+{
+  PRINTF("HERE in init_lj\n");
+  p->potential = LJ+Cu;
+  p->cutoff = 2.5*2.315;
+  p->lat = 3.615;
+  p->lattice_type=FCC;
+  p->atomic_no = 29;
+  p->lj.epsilon = 0.167*4;
+  p->lj.sigma = 2.315;
+  p->force_edt = ljforce_edt;
+  p->forcevel_edt = ljforcevel_edt;
+  p->print = print_lj;
+
+  mass_t* mass_p;
+  ocrDbCreate(&p->mass, (void**)&mass_p, sizeof(mass_t), DB_PROP_NONE, PICK_1_1(NULL_HINT,NULL_GUID), NO_ALLOC);
+  mass_p->mass[0] = 63.55 * amu2internal_mass;
+  mass_p->inv_mass[0] = 1.0/mass_p->mass[0];
+  mass_p->inv_mass_2[0] = 0.5/mass_p->mass[0];
+  mass_p->inv_mass_dt[0] = dt/mass_p->mass[0];
+
+  p->massFloat = mass_p->mass[0];
+
+  ocrDbRelease(p->mass);
+
+}
