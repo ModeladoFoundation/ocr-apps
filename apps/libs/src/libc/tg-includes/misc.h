761611ba9e132b7d1722178e438e01ccf140f8ad
Ivan Ganev <ivan.b.ganev@intel.com>
2016-08-25 17:49:44 -0700

    DMA engine requires canonical addresses.
    FSIM newly enforces canonical address requirements for the DMA engine
    (same as the hardware will), so we need to make sure each DMA request
    LEAs both source & destination addresses before issuing.
    
    Change-Id: Iad11b2d3488dee51f4b9f1441175d2b10d86b124

--- apps/libs/src/libc/tg-includes/misc.h
+++ apps/libs/src/libc/tg-includes/misc.h
@@ -11,12 +11,15 @@
     do { __asm__ __volatile__("fence 0xF, B\n\t"); } while(0)
 
 #define hal_memCopy(destination, source, size, isBackground)            \
-    do { __asm__ __volatile__("dma.copy %0, %1, %2, 0, 8\n\t"           \
+    do { __asm__ __volatile__("lea %0, %0\n\t"                          \
+                              "lea %1, %1\n\t"                          \
+                              "dma.copy %0, %1, %2, 0, 8\n\t"           \
                               :                                         \
                               : "r" ((void *)(destination)),            \
                                 "r" ((void *)(source)),                 \
-                                "r" (size));                            \
-        if (!isBackground) __asm__ __volatile__("fence 0xF, B\n\t");    \
+                                "r" (size)                              \
+                              : "0", "1", "memory");                    \
+        if (!isBackground) __asm__ __volatile__("fence 0x8, B\n\t" : : : "memory");    \
     } while(0)
 
 #endif // _MISC_H_
