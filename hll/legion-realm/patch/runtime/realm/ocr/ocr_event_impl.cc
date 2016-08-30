bc09b8d7d780fa147479155c5d9ecf4f4f23398a
srpaul <sriraj@rice.edu>
2016-08-29 23:04:42 -0500

    Realm: replacing dummy EDT with a latch for event merging
    To merge a set of events, a dummy EDT was created
    with the events as its depv. Replaced the EDT with a
    latch event, whose increment slot is set to number
    of events to merge and the events are added as
    dependences to the decrement slot.
    
    Change-Id: I0c31ce752cbb5804ed1fb503100345a737ddedc3

--- hll/legion-realm/patch/runtime/realm/ocr/ocr_event_impl.cc
+++ hll/legion-realm/patch/runtime/realm/ocr/ocr_event_impl.cc
@@ -33,28 +33,14 @@ namespace Realm {
 
     extern Logger log_event; // in event_impl.cc
 
-    ocrGuid_t OCREventImpl::merge_events_edt_t = NULL_GUID;
     const ID OCREventImpl::ocr_event_impl = ID(OCREventImpl::ID_TYPE, gasnet_mynode(), 0);
-
-    //This function is used to create a dummy EDT so that it waits till all dependencies are met.
-    //This is used to merge events i.e create an event that waits for all the input event to be met
-    ocrGuid_t ocr_realm_merge_events_func(u32 argc, u64 *argv, u32 depc, ocrEdtDep_t depv[])
-    {
-        assert(argc==0 && argv==NULL);
-        assert(depc >= 1);
-        return NULL_GUID;
-    }
 
     /*static*/ void OCREventImpl::static_init(void)
     {
-      //create the merge events edt template
-      ocrEdtTemplateCreate(&OCREventImpl::merge_events_edt_t, ocr_realm_merge_events_func, 0, EDT_PARAM_UNK);
     }
 
     /*static*/ void OCREventImpl::static_destroy(void)
     {
-      //delete the merge events edt template
-      ocrEdtTemplateDestroy(OCREventImpl::merge_events_edt_t);
     }
 
     /*static*/ Event OCREventImpl::create_ocrevent(void)
@@ -80,7 +66,7 @@ namespace Realm {
       OCREventImpl::wait(evt);
     }
 
-    //create an EDT with an array of wait_for events as dependency
+    //create a latch event and add the set of events as its decr slot dependencies
     /*static*/Event OCREventImpl::merge_events(const std::set<Event>& wait_for)
     {
       if (wait_for.empty())
@@ -89,84 +75,67 @@ namespace Realm {
         return *(wait_for.begin());
       else
       {
-        const unsigned size = wait_for.size();
-        ocrGuid_t wait_for_arr[size], merge_events_edt, out_merge_events_edt;
-
+        ocrEventParams_t params;
+        params.EVENT_LATCH.counter = wait_for.size();
+        ocrGuid_t latchguid;
+        u8 ret = ocrEventCreateParams(&latchguid, OCR_EVENT_LATCH_T, false, &params);
         Event ret_evt = OCREventImpl::create_ocrevent();
-
-        //convert Event set to an array of guids and pass it to EDT as dependency
-        std::set<Event>::iterator it; int i;
-        for(it = wait_for.begin(), i=0; it!=wait_for.end(); it++,i++)
-        {
-          wait_for_arr[i] = it->evt_guid;
-        }
-        wait_for_arr[0] = UNINITIALIZED_GUID;
-
-        ocrEdtCreate(&merge_events_edt, OCREventImpl::merge_events_edt_t, EDT_PARAM_DEF, NULL, size,
-                     wait_for_arr, EDT_PROP_NONE, NULL_HINT, &out_merge_events_edt);
-
-        //attach the sticky finish event to the edt since legacy_block_progress needs persistent event
-        ocrAddDependence(out_merge_events_edt, ret_evt.evt_guid, 0, DB_MODE_RO);
-
-        //start the EDT by statisfying dependency only after linking to the return event
-        ocrAddDependence(wait_for.begin()->evt_guid, merge_events_edt, 0, DB_MODE_RO);
-
+        //attach the sticky finish event to the latch since legacy_block_progress needs persistent event
+        ocrAddDependence(latchguid, ret_evt.evt_guid, 0, DB_MODE_RO);
+        std::set<Event>::iterator it;
+        for(it = wait_for.begin(); it!=wait_for.end(); it++)
+          ocrAddDependence(it->evt_guid, latchguid, OCR_EVENT_LATCH_DECR_SLOT, DB_MODE_RO);
         return ret_evt;
       }
     }
 
-    //create an EDT with an array of ev* events as dependency
+    //create a latch event and add ev* as its decr slot dependencies
     /*static*/ Event OCREventImpl::merge_events(Event ev1, Event ev2,
                                                 Event ev3 /*= NO_EVENT*/, Event ev4 /*= NO_EVENT*/,
                                                 Event ev5 /*= NO_EVENT*/, Event ev6 /*= NO_EVENT*/)
     {
       const unsigned size=6;
-      ocrGuid_t wait_for_arr[size], merge_events_edt, out_merge_events_edt, tmp_first_event;
+      ocrGuid_t wait_for_arr[size];
       int count=0;
-
-      Event ret_evt = OCREventImpl::create_ocrevent();
 
       //convert Event parameters to an array of Guid and pass it to EDT as dependency
       if(ev1.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev1 << " index=" << 1;
+        log_event.info() << "event merging: event wait_on=" << ev1 << " index=" << 1;
          wait_for_arr[count++] =  ev1.evt_guid;
       }
       if(ev2.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev2 << " index=" << 2;
+        log_event.info() << "event merging: event wait_on=" << ev2 << " index=" << 2;
          wait_for_arr[count++] =  ev2.evt_guid;
       }
       if(ev3.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev3 << " index=" << 3;
+        log_event.info() << "event merging: event wait_on=" << ev3 << " index=" << 3;
          wait_for_arr[count++] =  ev3.evt_guid;
       }
       if(ev4.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev4 << " index=" << 4;
+        log_event.info() << "event merging: event wait_on=" << ev4 << " index=" << 4;
          wait_for_arr[count++] =  ev4.evt_guid;
       }
       if(ev5.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev5 << " index=" << 5;
+        log_event.info() << "event merging: event wait_on=" << ev5 << " index=" << 5;
          wait_for_arr[count++] =  ev5.evt_guid;
       }
       if(ev6.exists()) {
-        log_event.info() << "event merging: event=" << ret_evt << " wait_on=" << ev6 << " index=" << 6;
+        log_event.info() << "event merging: event wait_on=" << ev6 << " index=" << 6;
          wait_for_arr[count++] =  ev6.evt_guid;
       }
 
       if(count == 0)
         return Event::NO_EVENT;
 
-      tmp_first_event = wait_for_arr[0];
-      wait_for_arr[0] = UNINITIALIZED_GUID;
-
-      ocrEdtCreate(&merge_events_edt, OCREventImpl::merge_events_edt_t, EDT_PARAM_DEF, NULL, count,
-                   wait_for_arr, EDT_PROP_NONE, NULL_HINT, &out_merge_events_edt);
-
-      //attach the sticky finish event to the edt since legacy_block_progress needs persistent event
-      ocrAddDependence(out_merge_events_edt, ret_evt.evt_guid, 0, DB_MODE_RO);
-
-      //start the EDT by statisfying dependency only after linking to the return event
-      ocrAddDependence(tmp_first_event, merge_events_edt, 0, DB_MODE_RO);
-
+      ocrEventParams_t params;
+      params.EVENT_LATCH.counter = count;
+      ocrGuid_t latchguid;
+      u8 ret = ocrEventCreateParams(&latchguid, OCR_EVENT_LATCH_T, false, &params);
+      Event ret_evt = OCREventImpl::create_ocrevent();
+      //attach the sticky finish event to the latch since legacy_block_progress needs persistent event
+      ocrAddDependence(latchguid, ret_evt.evt_guid, 0, DB_MODE_RO);
+      for(int i=0; i<count; i++)
+        ocrAddDependence(wait_for_arr[i], latchguid, OCR_EVENT_LATCH_DECR_SLOT, DB_MODE_RO);
       return ret_evt;
     }
 
