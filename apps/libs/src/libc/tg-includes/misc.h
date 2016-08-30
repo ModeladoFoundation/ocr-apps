70d30433ee1140641f41e475a0ac70ca26150fdc
Romain <romain.e.cledat@intel.com>
2016-08-30 16:50:06 -0700

    Fixed TG DMA copy assembly
    Change-Id: I8f6c0a12ac50386f39547a15a507fd11e0cec422

--- apps/libs/src/libc/tg-includes/misc.h
+++ apps/libs/src/libc/tg-includes/misc.h
@@ -11,14 +11,17 @@
     do { __asm__ __volatile__("fence 0xF, B\n\t"); } while(0)
 
 #define hal_memCopy(destination, source, size, isBackground)            \
-    do { __asm__ __volatile__("lea %0, %0\n\t"                          \
-                              "lea %1, %1\n\t"                          \
-                              "dma.copy %0, %1, %2, 0, 8\n\t"           \
-                              :                                         \
-                              : "r" ((void *)(destination)),            \
-                                "r" ((void *)(source)),                 \
-                                "r" (size)                              \
-                              : "0", "1", "memory");                    \
+    do {                                                                \
+        volatile u64 __dst = (volatile u64)(destination);               \
+        volatile u64 __src = (volatile u64)(source);                    \
+        __asm__ __volatile__("lea %0, %0\n\t"                           \
+                             "lea %1, %1\n\t"                           \
+                             "dma.copy %0, %1, %2, 0, 8\n\t"            \
+                             :                                          \
+                             : "r" ((void *)(__dst)),                   \
+                               "r" ((void *)(__src)),                   \
+                               "r" (size)                               \
+                             : "memory");                               \
         if (!isBackground) __asm__ __volatile__("fence 0x8, B\n\t" : : : "memory");    \
     } while(0)
 
