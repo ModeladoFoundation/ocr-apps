96bdfdcd57882e15a257c1655e1f948c0de58f7b
Jorge Bellon Castro <jorge.bellon.castro@intel.com>
2016-10-03 16:42:25 -0700

    Include cleanup EDT for each OmpSs task.
    Cleanup EDT will take care of cleaning and releasing all resources
    that cannot be safely released after an EDT function is executed,
    due to the possibility of subsequent accesses by this task's
    successors.
    Cleanup task for main function is also responsible of calling
    ocrAbort/ocrShutdown.
    
    Change-Id: I0cf98f2c90e540d742c5691cdc8d96ba7c3d0a0d

--- hll/ompss/ompss/src/main.c
+++ hll/ompss/ompss/src/main.c
@@ -8,45 +8,26 @@
 #include "task.h"
 #include "task-local.h"
 
+ocrGuid_t shutdownTemplate;
+ocrGuid_t ompssMainTemplate;
+
 extern int ompss_user_main( int argc, char* argv[] );
 
-//! \brief Initialize the runtime at least to the point that it will accept tasks
-void nanos_preinit()
+struct main_local_storage
 {
-}
+    hash_table_t local_dependences;
+    int return_value;
+};
 
-//! \brief Continue with the rest of the runtime initialization
-void nanos_init()
-{
-    // Create EDT template
-    u8 err = ocrEdtTemplateCreate( &taskOutlineTemplate, edtOutlineWrapper, 1, EDT_PARAM_UNK );
-    ASSERT( err == 0);
-}
-
-//! \brief Wait until the the runtime has shut down
-void nanos_wait_until_shutdown()
-{
-    // Cleanup. For the moment EDT templates are
-    // used only once.
-    u8 err = ocrEdtTemplateDestroy( taskOutlineTemplate );
-    ASSERT( err == 0);
-}
-
-//! \brief Notify the runtime that it can begin the shutdown process
-void nanos_notify_ready_for_shutdown()
-{
-}
-
-ocrGuid_t mainEdt(
+ocrGuid_t edtUserMain(
             u32 paramc,
             u64* paramv,
             u32 depc,
             ocrEdtDep_t depv[] )
 {
-    nanos_preinit();
-    nanos_init();
+    struct main_local_storage* main_data = depv[0].ptr;
 
-    void* program_args = depv[0].ptr;
+    void* program_args = depv[1].ptr;
     u64 argc = getArgc( program_args );
     char* argv[argc];
     for( u64 arg = 0; arg < argc; ++arg ) {
@@ -61,9 +42,102 @@ ocrGuid_t mainEdt(
 
     nanos_wait_until_shutdown();
 
+    return NULL_GUID;
+}
+
+ocrGuid_t edtShutdown(
+            u32 paramc,
+            u64* paramv,
+            u32 depc,
+            ocrEdtDep_t depv[] )
+{
+    // Decode task dependencies
+    struct main_local_storage* tls = (struct main_local_storage*)(depv[0].ptr + sizeof(ocrGuid_t));
+    int ret = tls->return_value;
+
+    // Free task local data storage
+    destructHashTable( &tls->local_dependences );
+    ocrDbDestroy( depv[0].guid );
+
     // If ret == 0, this is equivalent to ocrShutdown
     ocrAbort(ret);
 
     return NULL_GUID;
+}
+
+ocrGuid_t mainEdt(
+            u32 paramc,
+            u64* paramv,
+            u32 depc,
+            ocrEdtDep_t depv[] )
+{
+    nanos_preinit();
+    nanos_init();
+
+    struct main_local_storage* tls =
+        (struct main_local_storage*)malloc( sizeof(struct main_local_storage) );
+
+    u8 err;
+    ocrGuid_t userMainEdt, shutdownEdt;
+    ocrGuid_t userMainDoneEvt;
+    ocrGuid_t userMainDeps[2] = { ((ocrGuid_t*)tls)[-1] , depv[0].guid };
+    err = ocrEdtCreate( &userMainEdt, ompssMainTemplate,
+                  0, NULL,
+                  2, userMainDeps,
+                  EDT_PROP_FINISH, NULL_HINT, &userMainDoneEvt );
+    ASSERT( err == 0 );
+
+    ocrGuid_t shutdownDeps[2] = { ((ocrGuid_t*)tls)[-1], userMainDoneEvt };
+    err = ocrEdtCreate( &shutdownEdt, shutdownTemplate,
+                  0, NULL,
+                  2, shutdownDeps,
+                  EDT_PROP_NONE, NULL_HINT, NULL );
+    ASSERT( err == 0 );
+
+    return NULL_GUID;
+}
+
+//! \brief Initialize the runtime at least to the point that it will accept tasks
+void nanos_preinit()
+{
+}
+
+//! \brief Continue with the rest of the runtime initialization
+void nanos_init()
+{
+    // Create EDT templates
+    u8 err = ocrEdtTemplateCreate( &taskOutlineTemplate, edtOutlineWrapper, 0, EDT_PARAM_UNK );
+    ASSERT( err == 0);
+
+    err = ocrEdtTemplateCreate( &cleanupTemplate, edtCleanup, 0, 2 );
+    ASSERT( err == 0);
+
+    err = ocrEdtTemplateCreate( &ompssMainTemplate, edtUserMain, 0, 2 );
+    ASSERT( err == 0);
+
+    err = ocrEdtTemplateCreate( &shutdownTemplate, edtShutdown, 0, 2 );
+    ASSERT( err == 0);
+}
+
+//! \brief Wait until the the runtime has shut down
+void nanos_wait_until_shutdown()
+{
+}
+
+//! \brief Notify the runtime that it can begin the shutdown process
+void nanos_notify_ready_for_shutdown()
+{
+    // Do the shutdown right here
+    u8 err = ocrEdtTemplateDestroy( taskOutlineTemplate );
+    ASSERT( err == 0 );
+
+    err = ocrEdtTemplateDestroy( cleanupTemplate );
+    ASSERT( err == 0 );
+
+    err = ocrEdtTemplateDestroy( ompssMainTemplate );
+    ASSERT( err == 0 );
+
+    err = ocrEdtTemplateDestroy( shutdownTemplate );
+    ASSERT( err == 0 );
 }
 
--- hll/ompss/ompss/src/outline.h
+++ hll/ompss/ompss/src/outline.h
@@ -3,8 +3,13 @@
 #define OUTLINE_H
 
 extern ocrGuid_t taskOutlineTemplate;
+extern ocrGuid_t cleanupTemplate;
 
 ocrGuid_t edtOutlineWrapper( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[] );
+
+ocrGuid_t edtCleanup( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[] );
+
+ocrGuid_t edtShutdown( u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[] );
 
 #endif // OUTLINE_H
 
