bc412b3efaeae2976a811f3f1e8cc62dc63e102a
Jorge Bellon Castro <jorge.bellon.castro@intel.com>
2016-10-03 16:48:29 -0700

    Updated printf example to use task dependences
    Change-Id: I21123f22bc1cbd2593cecce84f7c5c97a7ca3ffb

--- hll/ompss/examples/printf/printf.c
+++ hll/ompss/examples/printf/printf.c
@@ -1,8 +1,30 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#pragma omp task
-void say_hello( const char* additional_text )
+#ifndef _OMPSS
+#define PRINTF printf
+#define ompss_user_main main
+#endif
+
+char text[50];
+
+static inline int min( int a, int b )
+{
+    return a < b? a: b;
+}
+
+#pragma omp task out( [size]text )
+void produce( char* text, int size )
+{
+   const char* src = "This is an OmpSs task";
+   int src_size = strlen(src) + 1;
+   strncpy( text, src, min(size,src_size) );
+}
+
+#pragma omp task in( [size]additional_text )
+void say_hello( const char* additional_text, int size )
 {
    PRINTF("Hello, world! %s\n", additional_text);
 }
@@ -18,10 +40,13 @@ int ompss_user_main( int argc, char* argv[] )
    PRINTF("Starting ompss_user_main. Arguments passed are:\n");
    print_args(argc, argv);
 
-   const char* text = "This is an OmpSs task";
-   say_hello(text);
+   char* text = malloc( 50 * sizeof(char) );
+   produce( text, 50 );
+
+   say_hello(text, strlen(text)+1 );
 
    #pragma omp taskwait
+   free( text );
 
    return 0;
 }
