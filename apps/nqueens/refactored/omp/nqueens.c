2eb37ddf36918bf22b4a39f2b1ef6bfc341e5a56
Jorge Bellon Castro <jorge.bellon.castro@intel.com>
2016-10-07 16:22:53 -0700

    Add refactored OpenMP with bounded parallelism
    Parallelism (number of created tasks) is limited to reduce runtime
    library overhead impact on execution time.
    
    Change-Id: Iffde8280469e721920b6af38856c8fd106b2ef78

---
+++ apps/nqueens/refactored/omp/nqueens.c
@@ -0,0 +1,64 @@
+// \file nqueens.c
+// \author Jorge Bellon <jbellonc@intel.com>
+//
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <omp.h>
+
+// Computes Hamming-weight for an arbitrary integer
+static inline uint32_t NumberOfSetBits(uint32_t i)
+{
+    i = i - ((i >> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
+static void find_solutions( uint32_t* found, uint32_t all, uint32_t ldiag, uint32_t cols, uint32_t rdiag )
+{
+    if( cols != all ) {
+        uint32_t available = ~( ldiag | cols | rdiag ) & all;
+        uint32_t spot = available & (-available);
+
+        while( spot != 0 ) {
+            #pragma omp task final( NumberOfSetBits(cols) > 4 )
+            find_solutions( found, all, (ldiag|spot)<<1, cols|spot, (rdiag|spot)>>1 );
+
+            available = available - spot;
+            spot = available & (-available);
+        }
+    } else {
+        // A solution was found!
+        #pragma omp atomic
+        *found += 1;
+    }
+}
+
+void solve_nqueens( uint32_t n )
+{
+    uint32_t found = 0;
+
+    uint32_t all = (1 << n) - 1;
+    #pragma omp parallel default(shared)
+    #pragma omp single
+    {
+        find_solutions( &found, all, 0, 0, 0 );
+        #pragma omp taskwait
+    }
+
+    printf( "%d-queens; %dx%d; sols: %d\n", n, n, n, found );
+}
+
+int main ( int argc, char* argv[] )
+{
+    assert( argc == 2 );
+
+    uint32_t n = atoi( argv[1] );
+    assert( 0 < n && n < 31 );
+
+    solve_nqueens( n );
+    return 0;
+}
