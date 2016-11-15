6a6f21a0acfdc220d674b2ed07ae81fda67bf88c
Jorge Bellon Castro <jorge.bellon.castro@intel.com>
2016-11-15 13:54:17 -0800

    Delete cleanup EDTs completely
    Cleanup EDTs (mainEdt cleanup included) are no longer necessary.
    All the necessary information will be passed using EDT parameters,
    instead of using datablocks.
    
    Change-Id: Ic3f9a434f900790284963f0107cb45094f206d2e

--- hll/ompss/ompss/src/memory/serializer.h
+++ hll/ompss/ompss/src/memory/serializer.h
@@ -6,23 +6,6 @@
 
 namespace mem {
 
-template< typename T >
-T* align( void* ptr ) {
-    const uintptr_t p = reinterpret_cast<uintptr_t>(ptr);
-    const uintptr_t aligned = (p + alignof(T) - 1u) & (-alignof(T));
-    return reinterpret_cast<T*>(aligned);
-}
-
-// Returns the minimum value multiple of sizeof(T) bytes
-// that can hold the memory contents between first and last
-// pointers
-template< typename T, typename U, typename V >
-ptrdiff_t distance( const U* first, const V* last ) {
-    ptrdiff_t distance = reinterpret_cast<const uint8_t*>(first)
-                         - reinterpret_cast<const uint8_t*>(last);
-    return distance/sizeof(T) + (distance%sizeof(T) > 0? 1: 0);
-}
-
 struct Serializer {
     Serializer( void* base ) :
         _position(base)
--- hll/ompss/ompss/src/outline.h
+++ hll/ompss/ompss/src/outline.h
@@ -5,6 +5,8 @@
 #include "dependences.h"
 #include "task.h"
 
+#include <type_traits>
+
 namespace ompss {
 
 struct MainStorage {
@@ -16,12 +18,13 @@ struct MainStorage {
     }
 };
 
+// Check that MainStorage can be safely passed as an EDT parameter
+static_assert( std::is_trivially_copyable<MainStorage>::value == true,
+    "This class must fulfill all requirements to support plain copies (memcpy)" );
+
 extern ocrGuid_t taskOutlineTemplate;
-extern ocrGuid_t cleanupTemplate;
 
 ocrGuid_t edtOutlineWrapper( uint32_t paramc, uint64_t* paramv, uint32_t depc, ocrEdtDep_t depv[] );
-
-ocrGuid_t edtCleanup( uint32_t paramc, uint64_t* paramv, uint32_t depc, ocrEdtDep_t depv[] );
 
 ocrGuid_t edtShutdown( uint32_t paramc, uint64_t* paramv, uint32_t depc, ocrEdtDep_t depv[] );
 
