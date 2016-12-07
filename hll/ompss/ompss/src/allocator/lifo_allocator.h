c55f26cb509e986d976abb64f2f54e81fc66d560
Jorge Bellon Castro <jorge.bellon.castro@intel.com>
2016-12-06 16:39:10 -0800

    Use throw version of allocate in debug versions.
    Change-Id: Ifb19be6eecd0385c8f613be9b60e7810042a2ba4

--- hll/ompss/ompss/src/allocator/lifo_allocator.h
+++ hll/ompss/ompss/src/allocator/lifo_allocator.h
@@ -28,7 +28,18 @@ struct lifo_allocator {
     {
     }
 
+#ifndef RELEASE
+    Tp* allocate( std::size_t n, std::size_t align = alignof(Tp) ) {
+        Tp* result = allocate( std::nothrow, n, align );
+        if( !result )
+            throw std::bad_alloc();
+        return result;
+    }
+
+	Tp* allocate( const std::nothrow_t&, std::size_t n, std::size_t align = alignof(Tp) ) noexcept {
+#else
 	Tp* allocate( std::size_t n, std::size_t align = alignof(Tp) ) noexcept {
+#endif
         const ptrdiff_t align_req = std::max( align, alignof(ptrdiff_t) );
         const size_t    alloc_size = n * sizeof(Tp);
 
@@ -49,15 +60,6 @@ struct lifo_allocator {
         }
         return result;
 	}
-
-#ifndef RELEASE
-    Tp* allocate( std::size_t n, std::size_t align = alignof(Tp) ) {
-        Tp* result = allocate( std::nothrow, n, align );
-        if( !result )
-            throw std::bad_alloc();
-        return result;
-    }
-#endif
 
 	void deallocate( Tp* ptr, std::size_t n = 0u ) {
         ptrdiff_t offset = sizeof(ptrdiff_t) + reinterpret_cast<ptrdiff_t*>(ptr)[-1];
