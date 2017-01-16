9467bf06c93d2a3ba2bfb486d63688482f71e6f2
Sriram Aananthakrishnan <sriram.aananthakrishnan@intel.com>
2017-01-16 11:18:17 -0800

    (ocrtranslate) Multi-header file test input
    Change-Id: I6bb7845f4a2b4ef483a00eda67de9a325042c472

---
+++ hll/rose/tools/ocrTranslate/tests/multi_source_file1.h
@@ -0,0 +1,8 @@
+#ifndef _FILE1_H
+#define _FILE1_H
+
+#pragma ocr datablock begin DATABLOCK(DBK_hmem)
+int *h_mem;
+#pragma ocr datablock end
+
+#endif
---
+++ hll/rose/tools/ocrTranslate/tests/multi_source_file2.h
@@ -0,0 +1,8 @@
+#ifndef _FILE2_H
+#define _FILE2_H
+
+#pragma ocr datablock begin DATABLOCK(DBK_hdmem)
+extern double* hd_mem;
+#pragma ocr datablock end
+
+#endif
