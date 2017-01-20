5bf497f34ac468ebc85125e721232bbda7277bcc
Sriram Aananthakrishnan <sriram.aananthakrishnan@intel.com>
2017-01-19 16:20:07 -0800

    (ocrtranslate) AstBuilder interface
    1. Code for generating empty function templates for EDTs
    
    Change-Id: I7a971e78a3b15b2b361c60bbf170bd01c024db17

--- hll/rose/tools/ocrTranslate/src/ocrTranslateEngine.h
+++ hll/rose/tools/ocrTranslate/src/ocrTranslateEngine.h
@@ -27,9 +27,27 @@ class OcrTranslator {
   const OcrObjectManager& m_ocrObjectManager;
  private:
   void insertOcrHeaderFiles();
+  void outlineEdt(std::string, OcrEdtContextPtr edt);
  public:
   OcrTranslator(SgProject* project, const OcrObjectManager& ocrObjectManager);
+  void outlineEdts();
   void translate();
 };
+
+/**************
+ * AstBuilder *
+ **************/
+// High-level API for building fragments of AST for OCR
+namespace AstBuilder {
+  SgType* buildu32Type(SgScopeStatement* scope);
+  SgType* buildu64Type(SgScopeStatement* scope);
+  SgType* buildu64PtrType(SgScopeStatement* scope);
+  SgType* buildOcrEdtDepType(SgScopeStatement* scope);
+  SgType* buildOcrEdtDepArrType(SgScopeStatement* scope);
+  SgFunctionDeclaration* buildOcrEdtFuncDecl(std::string name, SgScopeStatement* scope);
+  SgFunctionParameterList* buildOcrEdtParams(SgFunctionDeclaration* edtdecl);
+  SgFunctionDeclaration* buildOcrEdt(std::string name, OcrEdtContextPtr edtContext);
+};
+
 
 #endif
