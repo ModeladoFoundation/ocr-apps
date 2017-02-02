76e560a0347bb4299dc6c8f3dfbc954688c47e78
Mark Wattier <mark.wattier@eqware.net>
2017-02-01 16:44:41 -0800

    Added math (libm) examples to legacy/tg-xe with regression tests.
    Tests for float and double have been added to legacy/tg-xe.
    There are also complex math tests for float and double.
    
    These are all built and tested by the regression tests.
    
    Change-Id: I495cb815dd1df0cf411675d2a88bcaadcd32ee7a

--- apps/tools/regression_tests/run-fsim-swtest-tests.sh
+++ apps/tools/regression_tests/run-fsim-swtest-tests.sh
@@ -25,6 +25,10 @@ tg_cdemo tg_cdemo.p tg_cdemo_nonewlib tg_cdemo_nonewlib.p
 tg_cxxdemo tg_cxxdemo.p tg_throw tg_throw.p
 legacy_cxxhello legacy_cxxhello.p
 legacy_hello legacy_hello.p legacy_iotest legacy_iotest.p
+legacy_float_tests legacy_float_tests.p
+legacy_double_tests legacy_double_tests.p
+legacy_float_complex_tests legacy_float_complex_tests.p
+legacy_double_complex_tests legacy_double_complex_tests.p
 PIE_fptr_simple PIE_fptr_simple.p PIE_multi_seg PIE_multi_seg.p
 TCO_tailcall1 TCO_tailcall1.p"
 
@@ -111,6 +115,82 @@ for TEST in $TESTS; do
       export WORKLOAD_INSTALL=$APPS_ROOT/legacy/tg-xe
       export FSIM_ARGS="-q -c $WORKLOAD_INSTALL/ccfg.cfg -- $WORKLOAD_INSTALL/$TEST_FILE $TEST_DIR"
       REGEXS+=("all good." "terminate alarm")
+    ;;
+    legacy_float_tests|legacy_float_tests.p)
+      export WORKLOAD_INSTALL=$APPS_ROOT/legacy/tg-xe
+      export FSIM_ARGS="-q -c $WORKLOAD_INSTALL/ccfg.cfg -- $WORKLOAD_INSTALL/$TEST_FILE"
+      REGEXS+=("float sqrtf(2.00000000) = 1.41421354")
+      REGEXS+=("float cosf(0.00000000) = 1.00000000")
+      REGEXS+=("float expf(0.00000000) = 1.00000000")
+      REGEXS+=("float cosf(0.78539819) = 0.70710677")
+      REGEXS+=("float cosf(1.57079637) = -0.00000004")
+      REGEXS+=("float sincosf(1.57079637) = 1.00000000 -0.00000004")
+      REGEXS+=("float ceilf(1.57079637) = 2.00000000")
+      REGEXS+=("float floorf(1.57079637) = 1.00000000")
+      REGEXS+=("float fabsf(-1.00000000) = 1.00000000")
+      REGEXS+=("float frexpf(2560.00000000) = 0.62500000 12")
+      REGEXS+=("float modff(3.14159274) = 0.14159274 3.00000000")
+      REGEXS+=("float remquof(5.00000000,2.00000000) = 1.00000000 2")
+      REGEXS+=("float fmaf(5.00000000,2.00000000,3.00000000) = 13.00000000")
+      REGEXS+=("float scalbnf(5.00000000,2) = 20.00000000")
+      REGEXS+=("float cosf(nan) = nan")
+      REGEXS+=("float ceilf(nan) = nan")
+      REGEXS+=("float floorf(nan) = nan")
+      REGEXS+=("float fabsf(nan) = nan")
+      REGEXS+=("float frexpf(nan) = nan 0")
+      REGEXS+=("float modff(nan) = nan nan")
+      REGEXS+=("float remquof(nan,nan) = nan 0")
+      REGEXS+=("float fmaf(nan,nan,nan) = nan")
+      REGEXS+=("float scalbnf(nan,0) = nan")
+      REGEXS+=("float atanf(inf) = 1.57079637")
+    ;;
+    legacy_double_tests|legacy_double_tests.p)
+      export WORKLOAD_INSTALL=$APPS_ROOT/legacy/tg-xe
+      export FSIM_ARGS="-q -c $WORKLOAD_INSTALL/ccfg.cfg -- $WORKLOAD_INSTALL/$TEST_FILE"
+      REGEXS+=("double sqrt(2.0000000000000000) = 1.4142135623730951")
+      REGEXS+=("double cos(0.0000000000000000) = 1.0000000000000000")
+      REGEXS+=("double exp(0.0000000000000000) = 1.0000000000000000")
+      REGEXS+=("double cos(0.7853981633974483) = 0.7071067811865476")
+      REGEXS+=("double cos(1.5707963267948966) = 0.0000000000000001")
+      REGEXS+=("double sincos(1.5707963267948966) = 1.0000000000000000 0.0000000000000001")
+      REGEXS+=("double ceil(1.5707963267948966) = 2.0000000000000000")
+      REGEXS+=("double floor(1.5707963267948966) = 1.0000000000000000")
+      REGEXS+=("double fabs(-1.0000000000000000) = 1.0000000000000000")
+      REGEXS+=("double frexp(2560.0000000000000000) = 0.6250000000000000 12")
+      REGEXS+=("double modf(3.1415926535897931) = 0.1415926535897931 3.0000000000000000")
+      REGEXS+=("double remquo(5.0000000000000000,2.0000000000000000) = 1.0000000000000000 2")
+      REGEXS+=("double fma(5.0000000000000000,2.0000000000000000,3.0000000000000000) = 13.0000000000000000")
+      REGEXS+=("double scalbnf(5.0000000000000000,2) = 20.0000000000000000")
+      REGEXS+=("double cos(nan) = nan")
+      REGEXS+=("double ceil(nan) = nan")
+      REGEXS+=("double floor(nan) = nan")
+      REGEXS+=("double fabs(nan) = nan")
+      REGEXS+=("double frexp(nan) = nan 0")
+      REGEXS+=("double modf(nan) = nan nan")
+      REGEXS+=("double remquo(nan,nan) = nan 0")
+      REGEXS+=("double fma(nan,nan,nan) = nan")
+      REGEXS+=("double scalbn(nan,0) = nan")
+      REGEXS+=("double atan(inf) = 1.5707963267948966")
+    ;;
+    legacy_float_complex_tests|legacy_float_complex_tests.p)
+      export WORKLOAD_INSTALL=$APPS_ROOT/legacy/tg-xe
+      export FSIM_ARGS="-q -c $WORKLOAD_INSTALL/ccfg.cfg -- $WORKLOAD_INSTALL/$TEST_FILE"
+      REGEXS+=("float complex ccosf(0.00000000 0.00000000) = 1.00000000 -0.00000000")
+      REGEXS+=("float complex cexpf(0.00000000 0.00000000) = 1.00000000 0.00000000")
+      REGEXS+=("float complex ccosf(0.00000000 3.14159274) = 11.59195423 -0.00000000")
+      REGEXS+=("float complex ccosf(3.14159274 0.00000000) = -1.00000000 0.00000000")
+      REGEXS+=("float complex conjf(3.14159274 0.00000000) = 3.14159274 -0.00000000")
+      REGEXS+=("float complex conjf(nan nan) = nan nan")
+    ;;
+    legacy_double_complex_tests|legacy_double_complex_tests.p)
+      export WORKLOAD_INSTALL=$APPS_ROOT/legacy/tg-xe
+      export FSIM_ARGS="-q -c $WORKLOAD_INSTALL/ccfg.cfg -- $WORKLOAD_INSTALL/$TEST_FILE"
+      REGEXS+=("double complex ccosf(0.0000000000000000 0.0000000000000000) = 1.0000000000000000 -0.0000000000000000")
+      REGEXS+=("double complex cexpf(0.0000000000000000 0.0000000000000000) = 1.0000000000000000 0.0000000000000000")
+      REGEXS+=("double complex ccosf(0.0000000000000000 3.1415926535897931) = 11.5919542312622070 -0.0000000000000000")
+      REGEXS+=("double complex ccosf(3.1415926535897931 0.0000000000000000) = -1.0000000000000000 0.0000000000000000")
+      REGEXS+=("double complex conjf(3.1415926535897931 0.0000000000000000) = 3.1415927410125732 -0.0000000000000000")
+      REGEXS+=("double complex conjf(nan nan) = nan nan")
     ;;
     PIE_fptr_simple|PIE_fptr_simple.p)
       export WORKLOAD_INSTALL=$TG_INSTALL/../xe-llvm/test/PIE
