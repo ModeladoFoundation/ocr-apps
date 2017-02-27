e6f2ee1a1db3e211fca24d076de73e9278b3a9d8
Chandra S. Martha <chandra.martha@intel.com>
2017-02-27 11:45:47 -0800

    HPCG: TG runs support
    * Added necessary files for the TG runs
    * Added scripts used for the final report data
    
    Change-Id: I8a9a53d370ad4aec87f02e124cb1d035143c6ef2

---
+++ apps/hpcg/refactored/ocr/intel/experiments/tg/tgExtractStats.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sizes=(16 32)
+iters=(1 4)
+BLOCKCOUNTS=(1 2 4)
+
+mkdir -p analyzed
+
+for BLOCKCOUNT in ${BLOCKCOUNTS[@]}; do
+for size in ${sizes[@]}; do
+for iter in ${iters[@]}; do
+    jobHeader="${size}_${iter}_${BLOCKCOUNT}"
+    mkdir -p analyzed/${jobHeader}
+    python $OCR_TOP/ocr/scripts/tgStats/tgStats.py $PWD/install_${jobHeader}/tg/logs
+    mv results/* analyzed/${jobHeader}/
+    echo ${jobHeader}
+    echo "instruction_breakdown.csv"
+    cat analyzed/${jobHeader}/instruction_breakdown.csv
+    echo "energy_breakdown.csv"
+    cat analyzed/${jobHeader}/energy_breakdown.csv
+    echo "net_traffic.csv"
+    cat analyzed/${jobHeader}/net_traffic.csv
+    echo "blockwise_net_traffic.csv"
+    cat analyzed/${jobHeader}/blockwise_net_traffic.csv
+done
+done
+done
