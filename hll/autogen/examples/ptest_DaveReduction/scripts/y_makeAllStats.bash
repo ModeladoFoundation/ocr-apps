9b0d7a2ac1b1afd82c3928bd3220f5ed5f7d0527
vcave <vincent.cave@intel.com>
2017-03-09 10:41:37 -0800

    Reduction stat extraction scripts
    - Check for validity of input. It so happens that
      sometimes the MPI output from multiple ranks becomes
      mangled leading to the stat extraction including
      pieces of words instead of the actual timings
    
    Change-Id: I6d0602125a20a513a3fb3c1e567caaf8539645ba

---
+++ hll/autogen/examples/ptest_DaveReduction/scripts/y_makeAllStats.bash
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+    echo 'USAGE: <exec> <input data file>'
+    echo '=This script takes the name of the input file and parse its content'
+    echo ' in order to calculates some statistics.'
+    echo '=The content of the file must be the log output of ptest_DaveReduction$.'
+fi
+
+OUTF='z_zz_temp'
+FNAME=$1
+
+#Processing lines
+#   INFO: TIME: ffjFinalEdt_fcn_time_mark= 1487053690878790
+#   INFO: TIME: ffjMainEdt_fcn_time_mark=  1487053690877934
+a="$(grep INFO $FNAME|grep mark|tr -s ' ' '\t'|cut -f4)"
+b=($a)
+c="$(( ${b[1]} - ${b[0]} ))"
+#dbg> echo $c
+TOTAL_RUN_TIME="$(echo "scale=6;$c/1000000" | bc)"
+echo "FNAME= $FNAME"
+echo "TotalRunTime= $TOTAL_RUN_TIME in seconds"
+echo "All other measurements in micro-seconds."
+
+#Processing lines for reduxA
+#   INFO: TIME: DR rank=2/8 reduxA,reduxB=288,83
+LABEL='reduxA'
+A="$(grep INFO $FNAME|grep $LABEL|tr -s ' ' '\t'|cut -f6|tr '=' '\n'|grep -v $LABEL|cut -d',' -f1)"
+#dbg> echo $A
+printf "%s\n" "${A[@]}" > $OUTF
+STATS="$(python y_calc_stats.py $OUTF)"
+printf '%16s  %s\n' "$LABEL" "$STATS"
+
+#Processing lines for reduxB
+#   INFO: TIME: DR rank=2/8 reduxA,reduxB=288,83
+LABEL='reduxB'
+A="$(grep INFO $FNAME|grep $LABEL|tr -s ' ' '\t'|cut -f6|tr '=' '\n'|grep -v $LABEL|cut -d',' -f2)"
+#dbg> echo $A
+printf "%s\n" "${A[@]}" > $OUTF
+STATS="$(python y_calc_stats.py $OUTF)"
+printf '%16s  %s\n' "$LABEL" "$STATS"
+
+#Processing lines for scatter
+#   INFO: TIME: rank=0/8 scatter,FORtransit= 94,648
+LABEL='scatter'
+A="$(grep INFO $FNAME|grep $LABEL|tr -s ' ' '\t'|cut -f5|tr '=' '\n'|grep -v $LABEL|cut -d',' -f1)"
+#dbg> echo $A
+printf "%s\n" "${A[@]}" > $OUTF
+STATS="$(python y_calc_stats.py $OUTF)"
+printf '%16s  %s\n' "$LABEL" "$STATS"
+
+#Processing lines for FORtransit
+#   INFO: TIME: rank=0/8 scatter,FORtransit= 94,648
+LABEL='FORtransit'
+A="$(grep INFO $FNAME|grep $LABEL|tr -s ' ' '\t'|cut -f5|tr '=' '\n'|grep -v $LABEL|cut -d',' -f2)"
+#dbg> echo $A
+printf "%s\n" "${A[@]}" > $OUTF
+STATS="$(python y_calc_stats.py $OUTF)"
+printf '%16s  %s\n' "$LABEL" "$STATS"
+
