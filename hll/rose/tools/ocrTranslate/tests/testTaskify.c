7aedb8a3e89bbb919bb7d89206729a9860f46b39
Sriram Aananthakrishnan <sriram.aananthakrishnan@intel.com>
2017-03-09 23:53:24 -0800

    (ocrtranslate) Test cases
    * Adding destroy clause to test case
    
    Change-Id: I11bc3dbdce8a6d239ed15ca33ef78b2f5ddb618c

--- hll/rose/tools/ocrTranslate/tests/testTaskify.c
+++ hll/rose/tools/ocrTranslate/tests/testTaskify.c
@@ -15,28 +15,31 @@ int main(int argc, char* argv[]) {
   int size  = atoi(argv[1]);
 
 #pragma ocr datablock begin DATABLOCK(DBK_in)
-  double* in;
+  int* in;
 #pragma ocr datablock end
-  in = (double*) malloc(size * sizeof(double));
+  in = (int*) malloc(size * sizeof(int));
   int iter;
 
 #pragma ocr task begin TASK(TASK_init)	\
   DEP_EVTs() DEP_DBKs(DBK_in) DEP_ELEMs(in:size, in:iter)
+  printf("TASK_init\n");
   time_t t;
   iter = 0;
   srand((unsigned) time(&t));
   for(iter = 0; iter < size; ++iter) {
-    in[iter] = (double)(rand() % 100)/7;
+    in[iter] = 10;
   }
 #pragma ocr task end OEVENT(OEVT_init)
 
 #pragma ocr task begin TASK(TASK_increment) \
   DEP_EVTs(OEVT_init) DEP_DBKs(DBK_in) DEP_ELEMs(in:size, in:iter)
+  printf("TASK_increment\n");
   for(iter = 0; iter < size; ++iter) in[iter] = in[iter]+1;
 #pragma ocr task end OEVENT(OEVT_increment)
 
 #pragma ocr task begin TASK(TASK_print) \
   DEP_EVTs(OEVT_increment) DEP_DBKs(DBK_in) DEP_ELEMs(in:size, in:iter)
+  printf("TASK_print\n");
   printf("[");
   for(iter = 0; iter < size; ) {
     printf("%d", in[iter++]);
@@ -48,3 +51,4 @@ int main(int argc, char* argv[]) {
 #pragma ocr shutdown DEP_EVTs(OEVT_print)
   return 0;
 }
+
