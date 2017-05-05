5e3ee2d765054e760267751eee50e426f9d4865e
Sriram Aananthakrishnan <sriram.aananthakrishnan@intel.com>
2017-05-05 10:42:14 -0700

    (ocrtranslate) Ast Info Management
    * Moved the Ast Info management from ocrTranslateEngine to its own file
    * Scope based tracking of datablock/events info
    
    Change-Id: Ic693673c4e7cf54c7235c9f4c80e92f2853bdbfb

---
+++ hll/rose/tools/ocrTranslate/src/ocrAstInfo.cpp
@@ -0,0 +1,326 @@
+/*****************************************
+ * Author: Sriram Aananthakrishnan, 2017 *
+ *****************************************/
+
+#include "sage3basic.h"
+#include "ocrAstInfo.h"
+#include "boost/make_shared.hpp"
+#include "logger.h"
+#include <string>
+
+using namespace std;
+
+
+/*********************
+ * Utility Functions *
+ *********************/
+SgVariableSymbol* GetVariableSymbol(std::string name, SgScopeStatement* currentScope) {
+  SgName vname(name);
+  SgVariableSymbol* vsymbol = SageInterface::lookupVariableSymbolInParentScopes(vname, currentScope);
+  assert(vsymbol);
+  return vsymbol;
+}
+
+SgVariableSymbol* GetVariableSymbol(SgVariableDeclaration* vdecl, string vname) {
+  SgVariableSymbol* v_sym = NULL;
+  SgName name(vname);
+  SgInitializedName* vsgn = vdecl->get_decl_item(name);
+  assert(vsgn);
+  SgScopeStatement* scope = vsgn->get_scope();
+  if(scope->symbol_exists(name)) {
+    v_sym = scope->lookup_variable_symbol(name);
+  }
+  else {
+    // Else look for the symbol in declarations scope
+    scope = vdecl->get_scope();
+    v_sym = scope->lookup_variable_symbol(name);
+  }
+  if(!v_sym) cerr << "SgVariableSymbol lookup failed\n";
+  assert(v_sym);
+  return v_sym;
+}
+
+SgVariableSymbol* GetVariableSymbol(SgInitializedName* vsgn) {
+  SgVariableSymbol* v_sym = NULL;
+  SgScopeStatement* scope = vsgn->get_scope();
+  SgName name = vsgn->get_name();
+  if(scope->symbol_exists(name)) {
+    v_sym = scope->lookup_variable_symbol(name);
+  }
+  else {
+    // Else look for the symbol in declarations scope
+    SgDeclarationStatement* vdecl = vsgn->get_declaration();
+    scope = vdecl->get_scope();
+    v_sym = scope->lookup_variable_symbol(name);
+  }
+  if(!v_sym) cerr << "SgVariableSymbol lookup failed\n";
+  assert(v_sym);
+  return v_sym;
+
+}
+
+/**************
+ * DbkAstInfo *
+ **************/
+DbkAstInfo::DbkAstInfo(string dbkname, string guidName, string ptrName)
+  : m_dbkname(dbkname),
+    m_dbkGuidName(guidName),
+    m_dbkPtrName(ptrName) {
+}
+
+string DbkAstInfo::getDbkGuidName() const {
+  return m_dbkGuidName;
+}
+
+string DbkAstInfo::getDbkPtrName() const {
+  return m_dbkPtrName;
+}
+
+string DbkAstInfo::str() const {
+  ostringstream oss;
+  oss << "[ocrGuid:" << m_dbkGuidName << ", ";
+  oss << "ptr: " << m_dbkPtrName << "]";
+  return oss.str();
+}
+
+/**************
+ * EvtAstInfo *
+ **************/
+EvtAstInfo::EvtAstInfo(string evtname, string evtGuidName)
+  : m_evtname(evtname), m_evtGuidName(evtGuidName) {
+}
+
+string EvtAstInfo::getEvtGuidName() const {
+  return m_evtGuidName;
+}
+
+/************************
+ * TaskOutliningAstInfo *
+ ************************/
+TaskOutliningAstInfo::TaskOutliningAstInfo(string name) : m_taskName(name) { }
+
+void TaskOutliningAstInfo::setDepElemStructDecl(SgClassDeclaration* depElemStructDecl) {
+  m_depElemStructDecl = depElemStructDecl;
+}
+
+void TaskOutliningAstInfo::setDepElemBaseType(SgType* depElemBaseType) {
+  m_depElemBaseType = depElemBaseType;
+}
+
+void TaskOutliningAstInfo::setDepElemTypedefType(SgType* depElemTypedefType) {
+  m_depElemTypedefType = depElemTypedefType;
+}
+
+void TaskOutliningAstInfo::setTaskFuncDecl(SgFunctionDeclaration* edtDecl) {
+  m_edtDecl = edtDecl;
+}
+
+SgClassDeclaration* TaskOutliningAstInfo::getDepElemStructDecl() const {
+  return m_depElemStructDecl;
+}
+
+SgClassDefinition* TaskOutliningAstInfo::getDepElemStructDefn() const {
+  SgClassDefinition* structDefn = m_depElemStructDecl->get_definition();
+  assert(structDefn);
+  return structDefn;
+}
+
+SgType* TaskOutliningAstInfo::getDepElemBaseType() const {
+  return m_depElemBaseType;
+}
+
+SgType* TaskOutliningAstInfo::getDepElemTypedefType() const {
+  return m_depElemTypedefType;
+}
+
+SgFunctionDeclaration* TaskOutliningAstInfo::getTaskFuncDecl() const {
+  return m_edtDecl;
+}
+
+string TaskOutliningAstInfo::str() const {
+  ostringstream oss;
+  oss << "[TaskOutliningAstInfo : " << m_taskName << "\n";
+  oss << "  " << AstDebug::astToString(m_edtDecl) << "\n]\n";
+  return oss.str();
+}
+
+TaskOutliningAstInfo::~TaskOutliningAstInfo() {
+  // Do not delete the SgNode pointers
+  // Nothing to cleanup here otherwise
+}
+
+/***************
+ * TaskAstInfo *
+ ***************/
+TaskAstInfo::TaskAstInfo(string name) : m_taskName(name) {
+  m_outliningInfo = boost::make_shared<TaskOutliningAstInfo>(m_taskName);
+}
+
+TaskAstInfo::TaskType TaskAstInfo::getTaskType() const {
+  return m_taskType;
+}
+
+TaskOutliningAstInfoPtr TaskAstInfo::getTaskOutliningAstInfo() const {
+  return m_outliningInfo;
+}
+
+// Get Functions for OutliningInfo
+SgClassDeclaration* TaskAstInfo::getDepElemStructDecl() const {
+  return m_outliningInfo->getDepElemStructDecl();
+}
+
+SgClassDefinition* TaskAstInfo::getDepElemStructDefn() const {
+  return m_outliningInfo->getDepElemStructDefn();
+}
+
+SgType* TaskAstInfo::getDepElemBaseType() const {
+  return m_outliningInfo->getDepElemBaseType();
+}
+
+SgType* TaskAstInfo::getDepElemTypedefType() const {
+  return m_outliningInfo->getDepElemTypedefType();
+}
+
+SgFunctionDeclaration* TaskAstInfo::getTaskFuncDecl() const {
+  return m_outliningInfo->getTaskFuncDecl();
+}
+
+// Set Functions for Outlining Info
+void TaskAstInfo::setDepElemStructDecl(SgClassDeclaration* depElemStructDecl) {
+  m_outliningInfo->setDepElemStructDecl(depElemStructDecl);
+}
+
+void TaskAstInfo::setDepElemBaseType(SgType* depElemBaseType) {
+  m_outliningInfo->setDepElemBaseType(depElemBaseType);
+}
+
+void TaskAstInfo::setDepElemTypedefType(SgType* depElemTypedefType) {
+  m_outliningInfo->setDepElemTypedefType(depElemTypedefType);
+}
+
+void TaskAstInfo::setTaskFuncDecl(SgFunctionDeclaration* edtDecl) {
+  m_outliningInfo->setTaskFuncDecl(edtDecl);
+}
+
+TaskAstInfo::~TaskAstInfo() {
+  // Nothing to destroy here
+}
+
+/**************
+ * EdtAstInfo *
+ **************/
+EdtAstInfo::EdtAstInfo(string edtname) : TaskAstInfo(edtname) { }
+
+string EdtAstInfo::getEdtTemplateGuidName() const {
+  return m_edtTemplGuidName;
+}
+
+string EdtAstInfo::getDepElemStructName() const {
+  return m_depElemStructName;
+}
+
+string EdtAstInfo::getEdtGuidName() const {
+  return m_edtGuidName;
+}
+
+void EdtAstInfo::setTemplGuidName(string edtTemplGuidName) {
+  m_edtTemplGuidName = edtTemplGuidName;
+}
+void EdtAstInfo::setDepElemStructName(string depElemStructName) {
+  m_depElemStructName = depElemStructName;
+}
+
+void EdtAstInfo::setEdtGuidName(string edtGuidName) {
+  m_edtGuidName = edtGuidName;
+}
+
+string EdtAstInfo::str() const {
+  ostringstream oss;
+  oss << "[EdtAstInfo: " << m_edtTemplGuidName << ", "
+      << m_depElemStructName << ", " << m_edtGuidName << "]";
+  return oss.str();
+}
+
+EdtAstInfo::~EdtAstInfo() { }
+
+/*****************************
+ * LoopControlEdtAstInfo *
+ *****************************/
+LoopControlEdtAstInfo::LoopControlEdtAstInfo(string name) : EdtAstInfo(name) { }
+
+SgBasicBlock* LoopControlEdtAstInfo::getBasicBlock() const {
+  return m_basicblock;
+}
+
+string LoopControlEdtAstInfo::getIterCompEvtGuidName() const {
+  return m_iterCompEvtGuidName;
+}
+
+void LoopControlEdtAstInfo::setBasicBlock(SgBasicBlock* basicblock) {
+  m_basicblock = basicblock;
+}
+
+void LoopControlEdtAstInfo::setCompEvtGuidName(string compEvtGuidName) {
+  m_iterCompEvtGuidName = compEvtGuidName;
+}
+
+std::string LoopControlEdtAstInfo::str() const {
+  ostringstream oss;
+  oss << "[LoopControlEdtAstInfo : " << m_taskName;
+  oss << "  " << AstDebug::astToString(m_basicblock) << "\n";
+  oss << m_iterCompEvtGuidName << "]";
+  return oss.str();
+}
+
+LoopControlEdtAstInfo::~LoopControlEdtAstInfo() {
+  // Nothing to cleanup here
+}
+
+/******************
+ * AstInfoManager *
+ ******************/
+AstInfoManager::AstInfoManager() { }
+
+DbkAstInfoPtr AstInfoManager::regDbkAstInfo(string dbkname, string dbkGuidName, string dbkPtrName, SgScopeStatement* scope) {
+  DbkAstInfoPtr dbkAstInfoPtr = boost::make_shared<DbkAstInfo>(dbkname, dbkGuidName, dbkPtrName);
+  m_dbkAstInfoMap.insertObjectPtr(dbkname, dbkAstInfoPtr, scope);
+  return dbkAstInfoPtr;
+}
+
+bool AstInfoManager::findEdtAstInfo(string edtName) const {
+  TaskAstInfoMap::const_iterator f = m_taskAstInfoMap.find(edtName);
+  return f != m_taskAstInfoMap.end();
+}
+
+EdtAstInfoPtr AstInfoManager::regEdtAstInfo(string edtName) {
+  EdtAstInfoPtr edtAstInfoPtr = boost::make_shared<EdtAstInfo>(edtName);
+  TaskAstInfoMapElem elem(edtName, edtAstInfoPtr);
+  m_taskAstInfoMap.insert(elem);
+  return edtAstInfoPtr;
+}
+
+EvtAstInfoPtr AstInfoManager::regEvtAstInfo(string evtname, string evtGuidName, SgScopeStatement* scope) {
+  EvtAstInfoPtr evtAstInfo = boost::make_shared<EvtAstInfo>(evtname, evtGuidName);
+  m_evtAstInfoMap.insertObjectPtr(evtname, evtAstInfo, scope);
+  return evtAstInfo;
+}
+
+DbkAstInfoPtr AstInfoManager::getDbkAstInfo(string dbkname, SgScopeStatement* scope) {
+  DbkAstInfoPtr dbkAstInfo = m_dbkAstInfoMap.getObjectPtr(dbkname, scope);
+  assert(dbkAstInfo);
+  return dbkAstInfo;
+}
+
+EdtAstInfoPtr AstInfoManager::getEdtAstInfo(string edtname) {
+  TaskAstInfoMap::iterator f = m_taskAstInfoMap.find(edtname);
+  assert(f != m_taskAstInfoMap.end());
+  EdtAstInfoPtr edtAstInfo = boost::dynamic_pointer_cast<EdtAstInfo>(f->second);
+  assert(edtAstInfo);
+  return edtAstInfo;
+}
+
+EvtAstInfoPtr AstInfoManager::getEvtAstInfo(string evtname, SgScopeStatement* scope) {
+  EvtAstInfoPtr evtAstInfo = m_evtAstInfoMap.getObjectPtr(evtname, scope);
+  assert(evtAstInfo);
+  return evtAstInfo;
+}
---
+++ hll/rose/tools/ocrTranslate/src/ocrAstInfo.h
@@ -0,0 +1,198 @@
+/*****************************************
+ * Author: Sriram Aananthakrishnan, 2017 *
+ *****************************************/
+#ifndef _OCRASTINFO_H
+#define _OCRASTINFO_H
+
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include "ocrSymbolTable.h"
+
+// We will do some book keeping for the generated AST fragments
+// during the translation phase.
+// Useful in accessing them at various points during translation
+// Motivation : We will need the GUIDs of datablock, edts and events
+// at various steps of generating the AST for objects
+
+/*********************
+ * Utility Functions *
+ *********************/
+SgVariableSymbol* GetVariableSymbol(std::string name, SgScopeStatement* currentScope);
+SgVariableSymbol* GetVariableSymbol(SgVariableDeclaration* vdecl, std::string vname);
+SgVariableSymbol* GetVariableSymbol(SgInitializedName* vsgn);
+
+/**************
+ * DbkAstInfo *
+ **************/
+/*!
+ * \brief DbkAstInfo stores the variable names for guid
+ * and ptr created by the translation.
+ *
+ * The symbol is looked up using the SageInterface::lookupSymbolInParentScopes(varname)
+ */
+class DbkAstInfo {
+  std::string m_dbkname;
+  std::string m_dbkGuidName;
+  std::string m_dbkPtrName;
+ public:
+  DbkAstInfo(std::string dbkname, std::string guidName, std::string ptrName);
+  std::string getDbkName() const;
+  std::string getDbkGuidName() const;
+  std::string getDbkPtrName() const;
+  std::string str() const;
+};
+typedef boost::shared_ptr<DbkAstInfo> DbkAstInfoPtr;
+
+/**************
+ * EvtAstInfo *
+ **************/
+class EvtAstInfo {
+  std::string m_evtname;
+  std::string m_evtGuidName;
+ public:
+  EvtAstInfo(std::string evtname, std::string evtGuidName);
+  std::string getEvtGuidName() const;
+};
+typedef boost::shared_ptr<EvtAstInfo> EvtAstInfoPtr;
+
+/************************
+ * TaskOutliningAstInfo *
+ ************************/
+/*!
+ * \brief TaskOutliningAstInfo holds the outlined AST components of an EDT
+ */
+class TaskOutliningAstInfo {
+  std::string m_taskName;
+  SgClassDeclaration* m_depElemStructDecl;
+  SgType* m_depElemBaseType;
+  SgType* m_depElemTypedefType;
+  SgFunctionDeclaration* m_edtDecl;
+ public:
+  TaskOutliningAstInfo(std::string name);
+  // set functions
+  void setDepElemStructDecl(SgClassDeclaration* depElemStructDecl);
+  void setDepElemBaseType(SgType* depElemBaseType);
+  void setDepElemTypedefType(SgType* depElemTypedefType);
+  void setTaskFuncDecl(SgFunctionDeclaration* edtDecl);
+  // get functions
+  SgClassDeclaration* getDepElemStructDecl() const;
+  SgClassDefinition* getDepElemStructDefn() const;
+  SgType* getDepElemBaseType() const;
+  SgType* getDepElemTypedefType() const;
+  SgFunctionDeclaration* getTaskFuncDecl() const;
+  std::string str() const;
+  ~TaskOutliningAstInfo();
+};
+typedef boost::shared_ptr<TaskOutliningAstInfo> TaskOutliningAstInfoPtr;
+
+/***************
+ * TaskAstInfo *
+ ***************/
+/*!
+ *\brief Base class for storing AST builder information
+ *
+ * TaskAstInfo holds a shared_ptr to TaskOutliningAstInfo
+ * All Edt AstInfo class must inherit TaskAstInfo to access its outlined EDT's AST components
+ */
+class TaskAstInfo {
+ public:
+  typedef enum {
+    e_TaskEdt,
+    e_TaskLoopIterControl,
+    e_TaskShutDown
+  } TaskType;
+ protected:
+  TaskType m_taskType;
+  std::string m_taskName;
+  TaskOutliningAstInfoPtr m_outliningInfo;
+ public:
+  TaskAstInfo(std::string name);
+  TaskType getTaskType() const;
+  TaskOutliningAstInfoPtr getTaskOutliningAstInfo() const;
+  // Get Functions for OutliningInfo
+  SgClassDeclaration* getDepElemStructDecl() const;
+  SgClassDefinition* getDepElemStructDefn() const;
+  SgType* getDepElemBaseType() const;
+  SgType* getDepElemTypedefType() const;
+  SgFunctionDeclaration* getTaskFuncDecl() const;
+  // Set Functions for Outlining Info
+  void setDepElemStructDecl(SgClassDeclaration* depElemStructDecl);
+  void setDepElemBaseType(SgType* depElemBaseType);
+  void setDepElemTypedefType(SgType* depElemTypedefType);
+  void setTaskFuncDecl(SgFunctionDeclaration* edtDecl);
+  virtual std::string str() const=0;
+  ~TaskAstInfo();
+};
+typedef boost::shared_ptr<TaskAstInfo> TaskAstInfoPtr;
+
+/**************
+ * EdtAstInfo *
+ **************/
+/*!
+ * \brief EdtAstInfo holds the variable names of parameters needed in setting up the EDT
+ *
+ * Store the variable names and use the SgSymbolTable for looking up the SgVariableSymbol.
+ * Working with the assumption that all variable declaration statements are inserted
+ * either in the current scope of EDT setup or in the parent scope.
+ * SageInterface::loopVariableSymbolInParentScopes() can then be used to get the variable symbols.
+ */
+class EdtAstInfo : public TaskAstInfo {
+  std::string m_edtTemplGuidName;
+  std::string m_depElemStructName;
+  std::string m_edtGuidName;
+ public:
+  EdtAstInfo(std::string edtname);
+  std::string getEdtTemplateGuidName() const;
+  std::string getDepElemStructName() const;
+  std::string getEdtGuidName() const;
+  void setTemplGuidName(std::string edtTemplGuidName);
+  void setDepElemStructName(std::string depElemStructName);
+  void setEdtGuidName(std::string edtGuidName);
+  std::string str() const;
+  ~EdtAstInfo();
+};
+typedef boost::shared_ptr<EdtAstInfo> EdtAstInfoPtr;
+
+/*****************************
+ * LoopControlEdtAstInfo *
+ *****************************/
+class LoopControlEdtAstInfo : public EdtAstInfo {
+  // We generate this basic block and it is used to insert statements later
+  SgBasicBlock* m_basicblock;
+  std::string m_iterCompEvtGuidName;
+ public:
+  LoopControlEdtAstInfo(std::string name);
+  SgBasicBlock* getBasicBlock() const;
+  std::string getIterCompEvtGuidName() const;
+  void setBasicBlock(SgBasicBlock* basicblock);
+  void setCompEvtGuidName(std::string compEvtGuidName);
+  std::string str() const;
+  ~LoopControlEdtAstInfo();
+};
+typedef boost::shared_ptr<LoopControlEdtAstInfo> LoopControlEdtAstInfoPtr;
+
+/******************
+ * AstInfoManager *
+ ******************/
+typedef std::map<std::string, TaskAstInfoPtr> TaskAstInfoMap;
+typedef std::pair<std::string, TaskAstInfoPtr> TaskAstInfoMapElem;
+class AstInfoManager {
+  OcrObjectSymbolTable<DbkAstInfo> m_dbkAstInfoMap;
+  OcrObjectSymbolTable<EvtAstInfo> m_evtAstInfoMap;
+  TaskAstInfoMap m_taskAstInfoMap;
+ public:
+  AstInfoManager();
+  bool findEdtAstInfo(std::string edtName) const;
+  DbkAstInfoPtr regDbkAstInfo(std::string dbkname, std::string dbkGuidName, std::string dbkPtrName, SgScopeStatement* scope);
+  EvtAstInfoPtr regEvtAstInfo(std::string evtName, std::string evtGuidName, SgScopeStatement* scope);
+  EdtAstInfoPtr regEdtAstInfo(std::string edtName);
+  LoopControlEdtAstInfoPtr regLoopControlEdtAstInfo(std::string edtName);
+
+  DbkAstInfoPtr getDbkAstInfo(std::string dbkname, SgScopeStatement* scope);
+  EvtAstInfoPtr getEvtAstInfo(std::string evtname, SgScopeStatement* scope);
+  EdtAstInfoPtr getEdtAstInfo(std::string edtname);
+  LoopControlEdtAstInfo getLoopControlEdtAstInfo(std::string edtName);
+  std::string EvtAstInfoMap2Str() const;
+};
+
+#endif
