c114784228027592c1551c0cd76bd285e8277992
Sriram Aananthakrishnan <sriram.aananthakrishnan@intel.com>
2017-05-23 17:52:26 -0700

    (ocrtranslate) tailrecursion tests
    * minor edits
    
    Change-Id: I4e53962b6625e2f1758fc35a0860f9761c8aae86

--- hll/rose/tools/ocrTranslate/tests/tailrecursion/tailrecursion_test1.c
+++ hll/rose/tools/ocrTranslate/tests/tailrecursion/tailrecursion_test1.c
@@ -22,18 +22,18 @@ int main(int argc, char* argv[]) {
     for(i=1, k=2; i < size; i++) {
       arr[i] = arr[i-1]+k;
     }
-  }
 
-#pragma ocr task TASK(TASK_Print) DEP_DBKS(DBK_ARR) \
-  DEP_EVTs(OEVT_VecInit) DEP_ELEMs(size) OEVENT(OEVT_Print)
-  {
-    int i;
-    for(i=0; i < size; i++) {
-      printf("arr[%d]=%d\n", i, arr[i]);
+#pragma ocr task TASK(TASK_Print) DEP_DBKS(DBK_ARR)		\
+  DEP_EVTs(OEVT_FOR) DEP_ELEMs(size) OEVENT(OEVT_Print)
+    {
+      int i;
+      for(i=0; i < size; i++) {
+	printf("arr[%d]=%d\n", i, arr[i]);
+      }
     }
   }
 
-#pragma ocr shutdown DEP_EVTs(OEVT_Print)
+#pragma ocr shutdown DEP_EVTs(OEVT_VecInit)
 
   return 0;
 }
